d36e5dedeebec6c4aa15b10fdba79a24c2188f96
Aral Roca <aral-rg@hotmail.com>
2023-09-04 12:04:25 +0200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { jsx, jsxs, jsxDEV, Fragment } from './jsx-runtime';
+
+export function renderToSring(JSXElement) {
+  return 'TODO'
+}
---
+++ src/jsx-runtime.ts
@@ -0,0 +1,4 @@
+export const jsx = null
+export const jsxs = null
+export const jsxDEV = null
+export const Fragment = null
