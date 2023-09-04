a51250bcd0b31c8aaa11d0e2636745ae5c4186c2
Aral Roca <aral-rg@hotmail.com>
2023-09-04 14:01:05 +0200

    feat: implement renderToString

--- bunrise/index.ts
+++ bunrise/index.ts
@@ -1,3 +1,48 @@
-export function renderToSring(JSXElement) {
-  return 'TODO'
+interface Props {
+  [key: string]: any;
+  children?: JSXNode;
+}
+
+type JSXNode = string | number | JSXElement;
+
+interface JSXElement {
+  type: string | JSXComponent;
+  props: Props;
+}
+
+type JSXComponent = (props: Props) => JSXNode;
+
+function renderAttributes(props: Props): string {
+  let attributes = '';
+
+  for (const prop in props) {
+    if (prop !== 'children') attributes += ` ${prop}="${props[prop]}"`;
+  }
+
+  return attributes;
+}
+
+function renderChildren(children: JSXNode | undefined): string {
+  const renderChild = (child: JSXNode | undefined) => {
+    if (typeof child === 'string') return child;
+    if (typeof child === 'object') return renderToString(child);
+    return '';
+  }
+
+  if (Array.isArray(children)) return children.map(renderChild).join('');
+
+  return renderChild(children);
+}
+
+export function renderToString(element: JSXElement): string {
+  if (typeof element.type === 'function') {
+    const jsx = element.type(element.props)
+    return typeof jsx === 'string' || typeof jsx === 'number' ? jsx.toString() : renderToString(jsx)
+  }
+
+  const attributes = renderAttributes(element.props)
+  const content = renderChildren(element.props.children)
+  const openTag = `<${element.type}${attributes}>`
+
+  return `${openTag}${content}</${element.type}>`
 }
---
+++ tests/renderToString.test.tsx
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "bun:test"
+import { renderToString } from "../bunrise"
+
+describe('renderToString', () => {
+  it('should render a simple JSX element', () => {
+    const element = <div>Hello World</div>
+    const result = renderToString(element)
+    const expected = '<div>Hello World</div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('should render a complex JSX element', () => {
+    const Component = ({ name, title }) => (
+      <div title={title}>
+        <h1>Hello {name}</h1>
+        <p>This is a paragraph</p>
+      </div>
+    )
+    const element = <Component name="World" title="Test" />
+    const result = renderToString(element)
+    const expected = '<div title="Test"><h1>Hello World</h1><p>This is a paragraph</p></div>'
+    expect(result).toEqual(expected)
+  })
+})
