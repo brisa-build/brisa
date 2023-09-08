807ebfd57e2fccfe293fa049ac8ed2ea74d8b91a
Aral Roca <aral-rg@hotmail.com>
2023-09-08 19:00:36 +0200

    fix: fix dev serve

--- cli.sh
+++ cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ]; then
-    NODE_ENV=development bun --hot node_modules/bunrise/out/cli/serve.js
+    NODE_ENV=development bun --hot node_modules/bunrise/out/cli/dev/serve.js
 elif [ "$1" = "build" ]; then
     NODE_ENV=production bun node_modules/bunrise/out/cli/build.js
 elif [ "$1" = "start" ]; then
