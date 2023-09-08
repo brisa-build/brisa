3fee4f714ac570f772143edf0844cf362b7d6b85
Aral Roca <aral-rg@hotmail.com>
2023-09-08 14:34:49 +0200

    fix: fix serve

--- cli.sh
+++ cli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ]; then
-    NODE_ENV=development bun --hot node_modules/bunrise/out/cli/serve.js
+    bun --hot node_modules/bunrise/out/cli/serve.js development
 elif [ "$1" = "build" ]; then
     bun node_modules/bunrise/out/cli/build.js
 elif [ "$1" = "start" ]; then
-    NODE_ENV=production bun node_modules/bunrise/out/cli/serve.js
+    bun node_modules/bunrise/out/cli/serve.js production
 else
     echo "error: script not found $1"
 fi
