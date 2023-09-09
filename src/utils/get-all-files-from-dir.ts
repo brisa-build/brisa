b024f315a3ff458f9d494678e2dfb26b18d24250
Aral Roca <aral-rg@hotmail.com>
2023-09-09 12:28:07 +0200

    chore: add tests of precompressAssets and getAllFilesFromDir

--- src/utils/get-all-files-from-dir.ts
+++ src/utils/get-all-files-from-dir.ts
@@ -1,11 +1,11 @@
-import { readdir } from 'node:fs/promises';
+import { readdir } from "node:fs/promises";
 
-export async function getFiles(dir: string): Promise<string[]> {
+export default async function getFilesFromDir(dir: string): Promise<string[]> {
   const dirents = await readdir(dir, { withFileTypes: true });
   const filePromises: Promise<string[]>[] = dirents.map(async (dirent) => {
-    const res = [dir, dirent.name].join('/');
+    const res = [dir, dirent.name].join("/");
 
-    if (dirent.isDirectory()) return await getFiles(res);
+    if (dirent.isDirectory()) return await getFilesFromDir(res);
     else return [res];
   });
 
--- src/utils/precompress-assets.ts
+++ src/utils/precompress-assets.ts
@@ -1,16 +1,18 @@
-import { gzipSync } from 'bun'
-import { getFiles } from './get-all-files-from-dir';
+import { gzipSync } from "bun";
+import getFilesFromDir from "./get-all-files-from-dir";
 
 // TODO: Support brotli when it's supported by Bun
 export default async function precompressAssets(assetsPath: string) {
-  const assets = await getFiles(assetsPath);
+  const assets = await getFilesFromDir(assetsPath);
 
-  await Promise.all(assets.map(async (asset) => {
-    if (asset.endsWith('.gz')) return;
+  await Promise.all(
+    assets.map(async (asset) => {
+      if (asset.endsWith(".gz")) return;
 
-    const assetContent = Bun.file(asset);
-    const gzip = gzipSync(new Uint8Array((await assetContent.arrayBuffer())));
+      const assetContent = Bun.file(asset);
+      const gzip = gzipSync(new Uint8Array(await assetContent.arrayBuffer()));
 
-    Bun.write(`${asset}.gz`, gzip);
-  }))
+      Bun.write(`${asset}.gz`, gzip);
+    }),
+  );
 }
