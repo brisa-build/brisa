a7bf2a060f4dd142e5db3185780ab80c9de0f304
Aral Roca <aral-rg@hotmail.com>
2023-09-10 19:48:12 +0200

    refactor: move browser scripts to browser-scripts folder

--- src/browser-scripts/replace-pending-to-success-nodes/index.ts
+++ src/browser-scripts/replace-pending-to-success-nodes/index.ts
@@ -1,22 +1,22 @@
 /// <reference lib="dom.iterable" />
 
 /**
- * The pending and success nodes follow a hierarchical structure, where each node is identified
- * by a parent-child relationship. For example, a pending node with an ID of 1:1 represents the
- * parent, its child node would be 1:1.1, and any subsequent child nodes would be identified as
- * 1:1.1.1, 1:1.1.2, and so on.
- */
-const sortByDepth = (a: Element, b: Element) =>
-  b.id.split(".").length - a.id.split(".").length;
-
-/**
  * Replace pending to success nodes. (Only executed in the browser)
  *
  * This is useful to display pending Components meanwhile is loading the rest
  * of the page via streaming. Until all the page is loaded, then replace the
  * pending Components to success Components.
  */
-function replacePendingToSuccessNodes() {
+(function replacePendingToSuccessNodes() {
+  /**
+   * The pending and success nodes follow a hierarchical structure, where each node is identified
+   * by a parent-child relationship. For example, a pending node with an ID of 1:1 represents the
+   * parent, its child node would be 1:1.1, and any subsequent child nodes would be identified as
+   * 1:1.1.1, 1:1.1.2, and so on.
+   */
+  const sortByDepth = (a: Element, b: Element) =>
+    b.id.split(".").length - a.id.split(".").length;
+
   const pendings = Array.from(document.querySelectorAll("[id^=P\\:]")).sort(
     sortByDepth,
   );
@@ -31,6 +31,4 @@ function replacePendingToSuccessNodes() {
 
   // This removes the script that has this code after it has been executed.
   document.getElementById("R:0")?.remove();
-}
-
-replacePendingToSuccessNodes();
+})();
