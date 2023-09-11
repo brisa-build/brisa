24cfd938e351976f8d6b49f8452c6efdb57b52bc
Aral Roca <aral-rg@hotmail.com>
2023-09-11 17:13:57 +0200

    fix: fix new hotreloading with streaming

---
+++ src/cli/dev-live-reload.tsx
@@ -0,0 +1,23 @@
+const LIVE_RELOAD_WEBSOCKET_PATH = "__bunrise_live_reload__";
+const LIVE_RELOAD_COMMAND = "reload";
+
+globalThis.ws?.send?.(LIVE_RELOAD_COMMAND);
+
+export function LiveReloadScript({
+  port,
+  children,
+}: {
+  port: number;
+  children: JSX.Element;
+}) {
+  const wsUrl = `ws://0.0.0.0:${port}/${LIVE_RELOAD_WEBSOCKET_PATH}`;
+
+  return (
+    <>
+      <script>
+        {`(new WebSocket("${wsUrl}")).onmessage = e => e.data === "${LIVE_RELOAD_COMMAND}" && location.reload();`}
+      </script>
+      {children}
+    </>
+  );
+}
