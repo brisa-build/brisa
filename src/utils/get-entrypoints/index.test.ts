14cad9519d7ca1421e4d1f80feb4c42586ca0d9a
Aral Roca <aral-rg@hotmail.com>
2023-09-11 10:31:38 +0200

    fix when api or pages does not exist

---
+++ src/utils/get-entrypoints/index.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from "bun:test";
+import path from "node:path";
+import getEntrypoints from ".";
+
+const pagesDir = path.join(import.meta.dir, "..", "__fixtures__", "pages");
+
+describe("utils", () => {
+  describe("getEntrypoints", () => {
+    it("should return an array", () => {
+      const entrypoints = getEntrypoints(pagesDir);
+      const expected = ["_404.ts", "somepage.ts", "[username].ts"].map(
+        (route) => path.join(pagesDir, route),
+      );
+      expect(entrypoints).toEqual(expected);
+    });
+
+    it("should return an empty array if the directory does not exist", () => {
+      const entrypoints = getEntrypoints("some/path");
+      expect(Array.isArray(entrypoints)).toBe(true);
+      expect(entrypoints.length).toBe(0);
+    });
+  });
+});
