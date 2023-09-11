9c7c2b4a9b183fd747d4eba484bf128e378251c4
Aral Roca <aral-rg@hotmail.com>
2023-09-11 10:52:21 +0200

    fix build

--- src/cli/build.ts
+++ src/cli/build.ts
@@ -10,16 +10,24 @@ const projectDir = getRootDir();
 const srcDir = path.join(projectDir, "src");
 const pagesDir = path.join(srcDir, "pages");
 const apiDir = path.join(srcDir, "api");
-const outdir = path.join(projectDir, "build");
+let outdir = path.join(projectDir, "build");
 const outAssetsDir = path.join(outdir, "public");
 const inAssetsDir = path.join(srcDir, "public");
 const pagesEntrypoints = getEntrypoints(pagesDir);
 const apiEntrypoints = getEntrypoints(apiDir);
 const entrypoints = [...pagesEntrypoints, ...apiEntrypoints];
+
+// This fix Bun build with only one entrypoint because it doesn't create the subfolder
+if (entrypoints.length === 1) {
+  const subfolder = entrypoints[0].includes(path.join(outdir, "api")) ? 'api' : 'pages';
+  outdir = path.join(outdir, subfolder);
+}
+
 const { success, logs, outputs } = await Bun.build({
   entrypoints,
   outdir,
   minify: true,
+  sourcemap: "external",
 });
 
 if (!success) {
