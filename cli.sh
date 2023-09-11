f22222d6d8238b1e8935748b5ff8c86711235827
Aral Roca <aral-rg@hotmail.com>
2023-09-11 07:44:32 +0200

    fix: temporal release to debug in vercel

--- cli.sh
+++ cli.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if [ "$1" = "dev" ]; then
-    NODE_ENV=development ~/.bun/bin/bun --hot node_modules/bunrise/out/cli/dev/serve.js
-elif [ "$1" = "build" ]; then
-    NODE_ENV=production ~/.bun/bin/bun node_modules/bunrise/out/cli/build.js
-elif [ "$1" = "start" ]; then
-    NODE_ENV=production ~/.bun/bin/bun node_modules/bunrise/out/cli/serve.js
+if command -v bun >/dev/null 2>&1; then
+  BUN_EXEC="bun"
 else
-    echo "error: script not found $1"
+  BUN_EXEC="~/.bun/bin/bun"
+fi
+
+if [ "$1" = "dev" ]; then
+  NODE_ENV=development $BUN_EXEC --hot node_modules/bunrise/out/cli/dev/serve.js
+elif [ "$1" = "build" ]; then
+  NODE_ENV=production $BUN_EXEC node_modules/bunrise/out/cli/build.js  
+elif [ "$1" = "start" ]; then
+  NODE_ENV=production $BUN_EXEC node_modules/bunrise/out/cli/serve.js
+else
+  echo "Error: script not found $1"
 fi
