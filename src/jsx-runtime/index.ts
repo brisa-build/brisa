74fb819d31cc58e31cc17083aeeb93ea75271a72
Aral Roca <aral-rg@hotmail.com>
2023-11-18 01:42:10 +0100

    fix(web-components): fix jsx-runtime

--- src/jsx-runtime/index.ts
+++ src/jsx-runtime/index.ts
@@ -1,34 +1,7 @@
 import type { Type, Props } from "../types/index";
 
-function escapeHTML(unsafeText: unknown) {
-  if (typeof unsafeText !== "string") return unsafeText ?? "";
-  let div = document.createElement("div");
-  div.innerText = unsafeText;
-  return div.innerHTML;
-}
-
 const Fragment = (props: Props) => props.children;
-const createNode = (type: Type, props: Props) => {
-  if (typeof window === "undefined") return { type, props };
-
-  let { children, ...restProps } = props;
-  const childrenString = Array.isArray(children)
-    ? children.map(escapeHTML).join("")
-    : escapeHTML(children);
-
-  // @ts-ignore
-  if (type.__isFragment) return childrenString;
-
-  let attributes = Object.entries(restProps)
-    .map(([key, value]) => `${key}="${value}"`)
-    .join(" ");
-
-  if (attributes) attributes = " " + attributes;
-
-  return {
-    toString: () => `<${type}${attributes}>${childrenString}</${type}>`,
-  };
-};
+const createNode = (type: Type, props: Props) => ({ type, props });
 
 Fragment.__isFragment = true;
 
