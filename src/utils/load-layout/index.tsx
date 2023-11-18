294703c62f53ba92dd2044c3c433e84fafd8bf03
Aral Roca <aral-rg@hotmail.com>
2023-11-18 22:37:33 +0100

    chore: improve default layout

--- src/utils/load-layout/index.tsx
+++ src/utils/load-layout/index.tsx
@@ -14,6 +14,7 @@ export default async function LoadLayout({
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <meta name="theme-color" content="#317EFB" />
           <title>Brisa</title>
         </head>
         <body>{children}</body>
