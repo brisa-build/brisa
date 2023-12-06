efdfd19369170e09f656a00a37488688d2dc6640
Aral Roca <aral-rg@hotmail.com>
2023-12-06 21:41:16 +0100

    feat(web-components): add component wrapper to SSR web-components

---
+++ src/utils/ssr-web-component/index.tsx
@@ -0,0 +1,32 @@
+import { RequestContext } from '../../types';
+
+type Props = {
+  Component: any;
+  selector: string;
+  [key: string]: any;
+}
+
+const voidFn = () => { };
+
+export default function SSRWebComponent({ Component, selector, ...props }: Props) {
+  let style = ''
+  let Selector = selector
+
+  return (
+    <Selector {...props}>
+      <template shadowrootmode="open">
+        {Component(props, {
+          state: (value: unknown) => ({ value }),
+          effect: voidFn,
+          onMount: voidFn,
+          derived: (fn: () => unknown) => ({ value: fn() }),
+          cleanup: voidFn,
+          css: (strings: string[], ...values: string[]) => {
+            style += strings[0] + values.join("");
+          }
+        } as unknown as RequestContext)}
+        {style.length > 0 && <style>{style}</style>}
+      </template>
+    </Selector>
+  )
+}
