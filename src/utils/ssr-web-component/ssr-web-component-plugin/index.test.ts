9c36a3a34f2049e3da29cd6014d1f1f8e5d191ad
Aral Roca <aral-rg@hotmail.com>
2023-12-07 22:40:05 +0100

    fix(ssr-web-component): use same import duplicated components

--- src/utils/ssr-web-component/ssr-web-component-plugin/index.test.ts
+++ src/utils/ssr-web-component/ssr-web-component-plugin/index.test.ts
@@ -94,5 +94,39 @@ describe("utils", () => {
 
       expect(output).toEqual(expected);
     });
+
+    it("should use the same name if the web-component is used more than once", () => {
+      const code = `
+        export default function ServerComponent() {
+          return (
+            <>
+              <web-component />
+              <web-component />
+            </>
+          );
+        }
+      `;
+      const allWebComponents = {
+        "web-component": "src/components/web-component.tsx",
+      };
+      const output = normalizeQuotes(
+        wrapWithSSRWebComponent(code, allWebComponents).code
+      );
+      const expected = toExpected(`
+        import {SSRWebComponent as _Brisa_SSRWebComponent} from "brisa/server";
+        import _Brisa_WC1 from "src/components/web-component.tsx";
+
+        export default function ServerComponent() {
+          return (
+            <>
+              <_Brisa_SSRWebComponent Component={_Brisa_WC1} selector="web-component" />
+              <_Brisa_SSRWebComponent Component={_Brisa_WC1} selector="web-component" />
+            </>
+          );
+        }
+      `);
+
+      expect(output).toEqual(expected);
+    });
   });
 });
