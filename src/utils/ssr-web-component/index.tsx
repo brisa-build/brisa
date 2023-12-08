8631555e905725076f12ecf754065d7c35e889ae
Aral Roca <aral-rg@hotmail.com>
2023-12-08 21:52:01 +0100

    feat(web-components): add suspense and error during HTML streaming

--- src/utils/ssr-web-component/index.tsx
+++ src/utils/ssr-web-component/index.tsx
@@ -9,27 +9,44 @@ type Props = {
 
 const voidFn = () => {};
 
-export default function SSRWebComponent({
-  Component,
-  selector,
-  ...props
-}: Props) {
+export default async function SSRWebComponent(
+  { Component, selector, ...props }: Props,
+  requestContext: RequestContext
+) {
   let style = "";
   let Selector = selector;
+
+  const webContext = {
+    state: (value: unknown) => ({ value }),
+    effect: voidFn,
+    onMount: voidFn,
+    derived: (fn: () => unknown) => ({ value: fn() }),
+    cleanup: voidFn,
+    css: (strings: string[], ...values: string[]) => {
+      style += strings[0] + values.join("");
+    },
+  } as unknown as RequestContext;
+
+  const componentProps = { ...props, children: <slot /> };
+
+  let content: any;
+
+  try {
+    content = await (typeof Component.suspense === "function"
+      ? Component.suspense(componentProps, requestContext)
+      : Component(componentProps, webContext));
+  } catch (err) {
+    if (Component.error) {
+      content = await Component.error(err, componentProps, webContext);
+    } else {
+      throw err;
+    }
+  }
 
   return (
     <Selector {...props}>
       <template shadowrootmode="open">
-        {Component({ ...props, children: <slot /> }, {
-          state: (value: unknown) => ({ value }),
-          effect: voidFn,
-          onMount: voidFn,
-          derived: (fn: () => unknown) => ({ value: fn() }),
-          cleanup: voidFn,
-          css: (strings: string[], ...values: string[]) => {
-            style += strings[0] + values.join("");
-          },
-        } as unknown as RequestContext)}
+        {content}
         {style.length > 0 && <style>{toInline(style)}</style>}
       </template>
       {props.children}
