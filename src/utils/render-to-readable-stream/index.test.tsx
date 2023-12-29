c1f3f63f87630379060fa44b79c61c611fbe3a40
Aral Roca <aral-rg@hotmail.com>
2023-12-29 11:19:31 +0100

    test: add failing test to ignore slotted content

--- src/utils/render-to-readable-stream/index.test.tsx
+++ src/utils/render-to-readable-stream/index.test.tsx
@@ -1300,6 +1300,144 @@ describe("brisa core", () => {
       );
     });
 
+    it("should ignore slotted content when there is a div wrapper without slot attribute", () => {
+      type Theme = { color: string };
+      const ThemeCtx = createContext<Theme>({ color: "yellow" });
+
+      function ThemeProvider({ color }: Theme) {
+        return (
+          <>
+            <context-provider context={ThemeCtx} value={{ color }}>
+              <slot name="with-theme" />
+            </context-provider>
+            <slot />
+          </>
+        );
+      }
+
+      function ChildComponent({}, { useContext }: RequestContext) {
+        const context = useContext(ThemeCtx);
+        return <div>{context.value.color}</div>;
+      }
+
+      const stream = renderToReadableStream(
+        <SSRWebComponent
+          Component={ThemeProvider}
+          selector="theme-provider"
+          color="red"
+        >
+          <div>
+            <SSRWebComponent
+              Component={ChildComponent}
+              selector="child-component"
+            ></SSRWebComponent>
+            <SSRWebComponent
+              Component={ChildComponent}
+              selector="child-component"
+              slot="with-theme"
+            ></SSRWebComponent>
+          </div>
+        </SSRWebComponent>,
+        testRequest,
+      );
+
+      const result = Bun.readableStreamToText(stream);
+
+      expect(result).resolves.toBe(
+        toInline(`
+          <theme-provider color="red">
+            <template shadowrootmode="open">
+              <context-provider context="{'defaultValue':{'color':'yellow'}}" value="{'color':'red'}">
+                <slot name="with-theme"></slot>
+              </context-provider>
+              <slot></slot>
+            </template>
+            <div>
+              <child-component>
+                <template shadowrootmode="open">
+                  <div>yellow</div>
+                </template>
+              </child-component>
+              <child-component slot="with-theme">
+                <template shadowrootmode="open">
+                  <div>yellow</div>
+                </template>
+              </child-component>
+            </div>
+          </theme-provider>
+        `),
+      );
+    });
+
+    it("should apply slotted content when there is a div wrapper with slot attribute", () => {
+      type Theme = { color: string };
+      const ThemeCtx = createContext<Theme>({ color: "yellow" });
+
+      function ThemeProvider({ color }: Theme) {
+        return (
+          <>
+            <context-provider context={ThemeCtx} value={{ color }}>
+              <slot name="with-theme" />
+            </context-provider>
+            <slot />
+          </>
+        );
+      }
+
+      function ChildComponent({}, { useContext }: RequestContext) {
+        const context = useContext(ThemeCtx);
+        return <div>{context.value.color}</div>;
+      }
+
+      const stream = renderToReadableStream(
+        <SSRWebComponent
+          Component={ThemeProvider}
+          selector="theme-provider"
+          color="red"
+        >
+          <div slot="with-theme">
+            <SSRWebComponent
+              Component={ChildComponent}
+              selector="child-component"
+            ></SSRWebComponent>
+            <SSRWebComponent
+              Component={ChildComponent}
+              selector="child-component"
+              slot="with-theme"
+            ></SSRWebComponent>
+          </div>
+        </SSRWebComponent>,
+        testRequest,
+      );
+
+      const result = Bun.readableStreamToText(stream);
+
+      expect(result).resolves.toBe(
+        toInline(`
+          <theme-provider color="red">
+            <template shadowrootmode="open">
+              <context-provider context="{'defaultValue':{'color':'yellow'}}" value="{'color':'red'}">
+                <slot name="with-theme"></slot>
+              </context-provider>
+              <slot></slot>
+            </template>
+            <div slot="with-theme">
+              <child-component>
+                <template shadowrootmode="open">
+                  <div>red</div>
+                </template>
+              </child-component>
+              <child-component slot="with-theme">
+                <template shadowrootmode="open">
+                  <div>red</div>
+                </template>
+              </child-component>
+            </div>
+          </theme-provider>
+        `),
+      );
+    });
+
     it('should work "useContext" method with context-provider array of slots with name and web-components (SSR)', () => {
       type Theme = { color: string };
       const ThemeCtx = createContext<Theme>({ color: "yellow" });
