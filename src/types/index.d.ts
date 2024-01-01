a94f50463f9efce9cab4bb31a988c262dc5307a7
Aral Roca <aral-rg@hotmail.com>
2024-01-01 12:34:22 +0100

    fix: add events type-checking

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1460,6 +1460,108 @@ declare global {
       zoomAndPan?: string | undefined;
     }
 
+    export type TargetedEvent<
+      Target extends EventTarget = EventTarget,
+      TypedEvent extends Event = Event,
+    > = Omit<TypedEvent, "currentTarget"> & {
+      readonly currentTarget: Target;
+    };
+
+    export type TargetedAnimationEvent<Target extends EventTarget> =
+      TargetedEvent<Target, AnimationEvent>;
+    export type TargetedClipboardEvent<Target extends EventTarget> =
+      TargetedEvent<Target, ClipboardEvent>;
+    export type TargetedCompositionEvent<Target extends EventTarget> =
+      TargetedEvent<Target, CompositionEvent>;
+    export type TargetedDragEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      DragEvent
+    >;
+    export type TargetedFocusEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      FocusEvent
+    >;
+    export type TargetedInputEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      InputEvent
+    >;
+    export type TargetedKeyboardEvent<Target extends EventTarget> =
+      TargetedEvent<Target, KeyboardEvent>;
+    export type TargetedMouseEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      MouseEvent
+    >;
+    export type TargetedPointerEvent<Target extends EventTarget> =
+      TargetedEvent<Target, PointerEvent>;
+    export type TargetedSubmitEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      SubmitEvent
+    >;
+    export type TargetedTouchEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      TouchEvent
+    >;
+    export type TargetedTransitionEvent<Target extends EventTarget> =
+      TargetedEvent<Target, TransitionEvent>;
+    export type TargetedUIEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      UIEvent
+    >;
+    export type TargetedWheelEvent<Target extends EventTarget> = TargetedEvent<
+      Target,
+      WheelEvent
+    >;
+    export type TargetedPictureInPictureEvent<Target extends EventTarget> =
+      TargetedEvent<Target, PictureInPictureEvent>;
+
+    export type EventHandler<E extends TargetedEvent> = {
+      bivarianceHack(event: E): void;
+    }["bivarianceHack"];
+
+    export type AnimationEventHandler<Target extends EventTarget> =
+      EventHandler<TargetedAnimationEvent<Target>>;
+    export type ClipboardEventHandler<Target extends EventTarget> =
+      EventHandler<TargetedClipboardEvent<Target>>;
+    export type CompositionEventHandler<Target extends EventTarget> =
+      EventHandler<TargetedCompositionEvent<Target>>;
+    export type DragEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedDragEvent<Target>
+    >;
+    export type FocusEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedFocusEvent<Target>
+    >;
+    export type GenericEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedEvent<Target>
+    >;
+    export type InputEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedInputEvent<Target>
+    >;
+    export type KeyboardEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedKeyboardEvent<Target>
+    >;
+    export type MouseEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedMouseEvent<Target>
+    >;
+    export type PointerEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedPointerEvent<Target>
+    >;
+    export type SubmitEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedSubmitEvent<Target>
+    >;
+    export type TouchEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedTouchEvent<Target>
+    >;
+    export type TransitionEventHandler<Target extends EventTarget> =
+      EventHandler<TargetedTransitionEvent<Target>>;
+    export type UIEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedUIEvent<Target>
+    >;
+    export type WheelEventHandler<Target extends EventTarget> = EventHandler<
+      TargetedWheelEvent<Target>
+    >;
+    export type PictureInPictureEventHandler<Target extends EventTarget> =
+      EventHandler<TargetedPictureInPictureEvent<Target>>;
+
     export interface DOMAttributes<Target extends EventTarget>
       extends BrisaDOMAttributes {
       // Image Events
