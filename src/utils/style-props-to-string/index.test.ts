06137d4be0aa0d6e5d9229bff26cdbacdce8d4b7
Aral Roca <aral-rg@hotmail.com>
2024-01-01 20:32:21 +0100

    feat(props): add stylePropsToString util

---
+++ src/utils/style-props-to-string/index.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "bun:test";
+import stylePropsToString from ".";
+
+describe("utils", () => {
+  describe("stylePropsToString", () => {
+    it("should convert style props to string", () => {
+      const styleProps = {
+        color: "red",
+        backgroundColor: "blue",
+        padding: "10px",
+        margin: "10px",
+        border: "1px solid black",
+      };
+      const output = stylePropsToString(styleProps);
+      const expected = `color:red;background-color:blue;padding:10px;margin:10px;border:1px solid black;`;
+
+      expect(output).toEqual(expected);
+    });
+  });
+});
---
+++ src/utils/style-props-to-string/index.ts
@@ -0,0 +1,6 @@
+export default function stylePropsToString(styleProps: JSX.CSSProperties): string {
+  return Object.entries(styleProps).reduce((acc, [key, value]) => {
+    const cssKey = key.replace(/([A-Z])/g, "-$1").toLowerCase();
+    return `${acc}${cssKey}:${value};`;
+  }, "");
+}
