e2552a6cd2fac7001ddf2c136291a57d857a765b
Aral Roca <aral-rg@hotmail.com>
2024-01-06 19:37:59 +0100

    fix: add client code in 500 page

--- src/__fixtures__/middleware.ts
+++ src/__fixtures__/middleware.ts
@@ -3,12 +3,16 @@ import { RequestContext } from "../types";
 export default async function middleware(request: RequestContext) {
   const url = new URL(request.finalURL);
 
-  if (url.pathname !== "/test") return;
+  if (url.searchParams.get("throws-error")) {
+    throw new Error("Some internal error");
+  }
 
-  return new Response("", {
-    status: 302,
-    headers: {
-      Location: "/",
-    },
-  });
+  if (url.searchParams.get("redirect")) {
+    return new Response("", {
+      status: 301,
+      headers: {
+        Location: "/" + request.i18n.locale,
+      },
+    });
+  }
 }
