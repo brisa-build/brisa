1e845e0bd9a66ea4c7f950b3c2ae66f6d459e422
Aral Roca <aral-rg@hotmail.com>
2024-01-11 10:27:21 +0100

    fix: compile first assets

--- src/utils/compile-all/index.ts
+++ src/utils/compile-all/index.ts
@@ -2,14 +2,13 @@ import compileAssets from "@/utils/compile-assets";
 import compileFiles from "@/utils/compile-files";
 
 export default async function compileAll() {
+  await compileAssets();
+
   const { success, logs } = await compileFiles();
 
   if (!success) {
     logs.forEach((log) => console.error(log));
-    return false;
   }
 
-  await compileAssets();
-
-  return true;
+  return success;
 }
