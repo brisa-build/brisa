1e703bbb4f9c1d91509c4a3b61542fa6d37e3904
Aral Roca <aral-rg@hotmail.com>
2024-01-15 15:37:24 +0100

    feat: get all i18n keys in client page code

--- src/utils/analyze-client-ast/index.ts
+++ src/utils/analyze-client-ast/index.ts
@@ -1,15 +1,20 @@
 import type { ESTree } from "meriyah";
+import { logWarning } from "../log/log-build";
+import AST from "@/utils/ast";
+
+const { generateCodeFromAST } = AST("tsx");
 
 export default async function analyzeClientAst(
   ast: ESTree.Program,
   allWebComponents: Record<string, string>,
 ) {
   const webComponents: Record<string, string> = {};
+  let i18nKeys = new Set<string>();
   let useSuspense = false;
   let useContextProvider = false;
   let useI18n = false;
 
-  JSON.stringify(ast, (key, value) => {
+  JSON.stringify(ast, function (key, value) {
     const webComponentSelector = value?.arguments?.[0]?.value ?? "";
     const webComponentPath = allWebComponents[webComponentSelector];
     const isCustomElement =
@@ -33,6 +38,41 @@ export default async function analyzeClientAst(
 
     useI18n ||= value?.type === "Identifier" && value?.name === "i18n";
 
+    if (
+      value?.type === "CallExpression" &&
+      ((value?.callee?.type === "Identifier" && value?.callee?.name === "t") ||
+        (value?.callee?.property?.type === "Identifier" &&
+          value?.callee?.property?.name === "t"))
+    ) {
+      if (value?.arguments?.[0]?.type === "Literal") {
+        i18nKeys.add(value?.arguments?.[0]?.value);
+      } else {
+        logWarning(
+          [
+            "Addressing Dynamic i18n Key Export Limitations",
+            "",
+            `Code: ${generateCodeFromAST(value)}`,
+            "",
+            "When using dynamic i18n keys like t(someVar) instead of",
+            `literal keys such as t('example'), exporting these keys`,
+            `in the client code becomes challenging.`,
+            "",
+            "Unfortunately, it is not feasible to export dynamic keys",
+            "directly.",
+            "",
+            "To address this, it is crucial to specify these keys at",
+            `the page level. Here's an example:`,
+            "",
+            `export const i18nKeys = ['footer.*'] // You can use Glob`,
+            "",
+            "If you have any questions or need further assistance,",
+            "feel free to contact us. We are happy to help!",
+          ],
+          "Docs: https://brisa.build/docs/building-your-application/routing/internationalization#translate-in-your-web-components",
+        );
+      }
+    }
+
     useSuspense ||=
       value?.type === "ExpressionStatement" &&
       value?.expression?.operator === "=" &&
@@ -41,5 +81,7 @@ export default async function analyzeClientAst(
     return value;
   });
 
-  return { webComponents, useSuspense, useContextProvider, useI18n };
+  if (!useI18n) i18nKeys = new Set();
+
+  return { webComponents, useSuspense, useContextProvider, useI18n, i18nKeys };
 }
