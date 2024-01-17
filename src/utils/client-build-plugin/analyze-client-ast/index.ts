5d4a20232bb5e1e7aa1573856c60bde76a22c06e
Aral Roca <aral-rg@hotmail.com>
2024-01-17 12:52:36 +0100

    fix: show warning of i18nKeys only when is not specified

--- src/utils/client-build-plugin/analyze-client-ast/index.ts
+++ src/utils/client-build-plugin/analyze-client-ast/index.ts
@@ -7,6 +7,8 @@ const { generateCodeFromAST } = AST("tsx");
 export default function analyzeClientAst(ast: ESTree.Program) {
   let i18nKeys = new Set<string>();
   let useI18n = false;
+  let logs: any[] = [];
+  let isDynamicKeysSpecified = false;
 
   JSON.stringify(ast, function (key, value) {
     useI18n ||= value?.type === "Identifier" && value?.name === "i18n";
@@ -20,29 +22,7 @@ export default function analyzeClientAst(ast: ESTree.Program) {
       if (value?.arguments?.[0]?.type === "Literal") {
         i18nKeys.add(value?.arguments?.[0]?.value);
       } else {
-        logWarning(
-          [
-            "Addressing Dynamic i18n Key Export Limitations",
-            "",
-            `Code: ${generateCodeFromAST(value)}`,
-            "",
-            "When using dynamic i18n keys like t(someVar) instead of",
-            `literal keys such as t('example'), exporting these keys`,
-            `in the client code becomes challenging.`,
-            "",
-            "Unfortunately, it is not feasible to export dynamic keys",
-            "directly.",
-            "",
-            "To address this, it is crucial to specify these keys at",
-            `web-component level. Here's an example:`,
-            "",
-            `MyWebComponent.i18nKeys = ['footer.*'] // You can use Glob`,
-            "",
-            "If you have any questions or need further assistance,",
-            "feel free to contact us. We are happy to help!",
-          ],
-          "Docs: https://brisa.build/docs/building-your-application/routing/internationalization#translate-in-your-web-components",
-        );
+        logs.push(value);
       }
     }
 
@@ -54,11 +34,38 @@ export default function analyzeClientAst(ast: ESTree.Program) {
     ) {
       for (let element of value?.right?.elements ?? []) {
         i18nKeys.add(element.value);
+        isDynamicKeysSpecified = true;
       }
     }
 
     return value;
   });
+
+  if (logs.length > 0 && !isDynamicKeysSpecified) {
+    logWarning(
+      [
+        "Addressing Dynamic i18n Key Export Limitations",
+        "",
+        `Code: ${logs.map(generateCodeFromAST).join(", ")}`,
+        "",
+        "When using dynamic i18n keys like t(someVar) instead of",
+        `literal keys such as t('example'), exporting these keys`,
+        `in the client code becomes challenging.`,
+        "",
+        "Unfortunately, it is not feasible to export dynamic keys",
+        "directly.",
+        "",
+        "To address this, it is crucial to specify these keys at",
+        `web-component level. Here's an example:`,
+        "",
+        `MyWebComponent.i18nKeys = ['footer.*'] // You can use Glob`,
+        "",
+        "If you have any questions or need further assistance,",
+        "feel free to contact us. We are happy to help!",
+      ],
+      "Docs: https://brisa.build/docs/building-your-application/routing/internationalization#translate-in-your-web-components",
+    );
+  }
 
   if (!useI18n) i18nKeys = new Set();
 
