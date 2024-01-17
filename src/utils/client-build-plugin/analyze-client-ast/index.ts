00a3240e7f78f850524742b2c5e7a1aa4d06437c
Aral Roca <aral-rg@hotmail.com>
2024-01-17 12:30:17 +0100

    feat: support i18nKeys at web-component level

--- src/utils/client-build-plugin/analyze-client-ast/index.ts
+++ src/utils/client-build-plugin/analyze-client-ast/index.ts
@@ -8,7 +8,7 @@ export default function analyzeClientAst(ast: ESTree.Program) {
   let i18nKeys = new Set<string>();
   let useI18n = false;
 
-  JSON.stringify(ast, (key, value) => {
+  JSON.stringify(ast, function (key, value) {
     useI18n ||= value?.type === "Identifier" && value?.name === "i18n";
 
     if (
@@ -34,15 +34,26 @@ export default function analyzeClientAst(ast: ESTree.Program) {
             "directly.",
             "",
             "To address this, it is crucial to specify these keys at",
-            `the page level. Here's an example:`,
+            `web-component level. Here's an example:`,
             "",
-            `export const i18nKeys = ['footer.*'] // You can use Glob`,
+            `MyWebComponent.i18nKeys = ['footer.*'] // You can use Glob`,
             "",
             "If you have any questions or need further assistance,",
             "feel free to contact us. We are happy to help!",
           ],
           "Docs: https://brisa.build/docs/building-your-application/routing/internationalization#translate-in-your-web-components",
         );
+      }
+    }
+
+    // Add dynamic keys from: MyWebComponent.i18nKeys = ['footer*'];
+    if (
+      value?.type === "AssignmentExpression" &&
+      value?.left?.property?.name === "i18nKeys" &&
+      value?.right?.type === "ArrayExpression"
+    ) {
+      for (let element of value?.right?.elements ?? []) {
+        i18nKeys.add(element.value);
       }
     }
 
