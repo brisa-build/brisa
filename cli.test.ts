4463c3a2292493131a54d6f6c21ae1bfa1afeb04
Aral Roca <aral-rg@hotmail.com>
2024-01-21 17:35:58 +0100

    test: coverage cli

---
+++ cli.test.ts
@@ -0,0 +1,517 @@
+import {
+  describe,
+  it,
+  expect,
+  beforeEach,
+  afterEach,
+  spyOn,
+  mock,
+  type Mock,
+} from "bun:test";
+import { main } from "./cli.cjs";
+import cp from "child_process";
+import path from "node:path";
+
+const FIXTURES = path.join(import.meta.dir, "src", "__fixtures__");
+
+let originalArgv: string[];
+let mockSpawnSync: Mock<typeof cp.spawnSync>;
+let mockExit: Mock<typeof process.exit>;
+let mockLog: Mock<typeof console.log>;
+let mockCwd: Mock<typeof process.cwd>;
+
+const BRISA_BUILD_FOLDER = path.join(FIXTURES, "build");
+
+const prodOptions = {
+  stdio: "inherit",
+  env: { ...process.env, NODE_ENV: "production", BRISA_BUILD_FOLDER },
+} as any;
+const devOptions = {
+  stdio: "inherit",
+  env: { ...process.env, NODE_ENV: "development", BRISA_BUILD_FOLDER },
+} as any;
+
+describe("Brisa CLI", () => {
+  beforeEach(() => {
+    mockCwd = spyOn(process, "cwd").mockImplementation(() => FIXTURES);
+    mockLog = spyOn(console, "log").mockImplementation(() => null as any);
+    mockExit = spyOn(process, "exit").mockImplementation(() => null as never);
+    mockSpawnSync = spyOn(cp, "spawnSync").mockImplementation(
+      () => ({ status: 0 }) as any,
+    );
+    originalArgv = process.argv.slice();
+    mock.module(path.join(FIXTURES, "brisa.config.ts"), () => ({
+      default: {
+        output: "server",
+      },
+    }));
+  });
+  afterEach(() => {
+    mockLog.mockRestore();
+    mockExit.mockRestore();
+    mockSpawnSync.mockRestore();
+    mockCwd.mockRestore();
+    process.argv = originalArgv.slice();
+  });
+
+  it("should display the --help options", async () => {
+    process.argv = ["bun", "brisa", "--help"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(1); // bun --version
+    expect(mockLog.mock.calls).toEqual([
+      ["Command not found"],
+      ["Usage: brisa <command> [options]"],
+      ["Commands:"],
+      [" dev           Start development server"],
+      [" build         Build for production"],
+      [" start         Start production server"],
+      ["Options:"],
+      [" --help        Show help"],
+      [" --port        Specify port (applicable for dev and start commands)"],
+    ]);
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+
+  it("should display --help when the command is not found", async () => {
+    process.argv = ["bun", "brisa", "not-found"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(1); // bun --version
+    expect(mockLog.mock.calls).toEqual([
+      ["Command not found"],
+      ["Usage: brisa <command> [options]"],
+      ["Commands:"],
+      [" dev           Start development server"],
+      [" build         Build for production"],
+      [" start         Start production server"],
+      ["Options:"],
+      [" --help        Show help"],
+      [" --port        Specify port (applicable for dev and start commands)"],
+    ]);
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+
+  it('should execute "brisa dev" command with default options', async () => {
+    process.argv = ["bun", "brisa", "dev"];
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [path.join(import.meta.dir, "out", "cli", "build.js"), "DEV"],
+      devOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bun",
+      [
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "3000",
+        "DEV",
+      ],
+      devOptions,
+    ]);
+  });
+
+  it('should execute "brisa dev" command with custom port', async () => {
+    process.argv = ["bun", "brisa", "dev", "--port", "5000"];
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [path.join(import.meta.dir, "out", "cli", "build.js"), "DEV"],
+      devOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bun",
+      [
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "5000",
+        "DEV",
+      ],
+      devOptions,
+    ]);
+  });
+
+  it('should return the help of "brisa dev" command', async () => {
+    process.argv = ["bun", "brisa", "dev", "--help"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(1); // bun --version
+    expect(mockLog.mock.calls).toEqual([
+      ["Usage: brisa dev [options]"],
+      ["Options:"],
+      [" -p, --port         Specify port"],
+      [" -d, --debug        Enable debug mode"],
+      [
+        " -s, --skip-desktop Skip open desktop app when 'output': 'desktop' in brisa.config.ts",
+      ],
+      [" --help             Show help"],
+    ]);
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+
+  it('should debug "brisa dev" command', async () => {
+    process.argv = ["bun", "brisa", "dev", "--debug"];
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [path.join(import.meta.dir, "out", "cli", "build.js"), "DEV"],
+      devOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bun",
+      [
+        "--inspect",
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "3000",
+        "DEV",
+      ],
+      devOptions,
+    ]);
+  });
+
+  it('should build a desktop app with "brisa dev" command and output=desktop', async () => {
+    process.argv = ["bun", "brisa", "dev"];
+
+    mock.module(path.join(FIXTURES, "brisa.config.ts"), () => ({
+      default: {
+        output: "desktop",
+      },
+    }));
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      ["i", "@tauri-apps/cli"],
+      devOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bunx",
+      [
+        "tauri",
+        "init",
+        "-A",
+        "test",
+        "-W",
+        "test",
+        "-D",
+        "../out",
+        "--dev-path",
+        "http://localhost:3000",
+        "--before-dev-command",
+        "bun dev -- -s",
+        "--before-build-command",
+        "bun run build -- -s",
+      ],
+      devOptions,
+    ]);
+
+    expect(mockSpawnSync.mock.calls[3]).toEqual([
+      "bunx",
+      ["tauri", "dev", "--port", "3000"],
+      devOptions,
+    ]);
+  });
+
+  it('should build a desktop app with "brisa dev" command and output=desktop in another port', async () => {
+    process.argv = ["bun", "brisa", "dev", "--port", "5000"];
+
+    mock.module(path.join(FIXTURES, "brisa.config.ts"), () => ({
+      default: {
+        output: "desktop",
+      },
+    }));
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      ["i", "@tauri-apps/cli"],
+      devOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bunx",
+      [
+        "tauri",
+        "init",
+        "-A",
+        "test",
+        "-W",
+        "test",
+        "-D",
+        "../out",
+        "--dev-path",
+        "http://localhost:5000",
+        "--before-dev-command",
+        "bun dev -- -s",
+        "--before-build-command",
+        "bun run build -- -s",
+      ],
+      devOptions,
+    ]);
+
+    expect(mockSpawnSync.mock.calls[3]).toEqual([
+      "bunx",
+      ["tauri", "dev", "--port", "5000"],
+      devOptions,
+    ]);
+  });
+
+  it('should skip desktop "brisa dev" command', async () => {
+    process.argv = ["bun", "brisa", "dev", "--skip-desktop"];
+
+    mock.module(path.join(FIXTURES, "brisa.config.ts"), () => ({
+      default: {
+        output: "desktop",
+      },
+    }));
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [path.join(import.meta.dir, "out", "cli", "build.js"), "DEV"],
+      devOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bun",
+      [
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "3000",
+        "DEV",
+      ],
+      devOptions,
+    ]);
+  });
+
+  it('should execute "brisa build" command with default options', async () => {
+    process.argv = ["bun", "brisa", "build"];
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [path.join(import.meta.dir, "out", "cli", "build.js"), "PROD"],
+      prodOptions,
+    ]);
+  });
+
+  it('should execute "brisa build --help" command', async () => {
+    process.argv = ["bun", "brisa", "build", "--help"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(1); // bun --version
+    expect(mockLog.mock.calls).toEqual([
+      ["Usage: brisa build [options]"],
+      ["Options:"],
+      [
+        " -s, --skip-desktop Skip open desktop app when 'output': 'desktop' in brisa.config.ts",
+      ],
+      [" --help             Show help"],
+    ]);
+  });
+
+  it('should build a desktop app with "brisa build" command and output=desktop', async () => {
+    process.argv = ["bun", "brisa", "build"];
+
+    mock.module(path.join(FIXTURES, "brisa.config.ts"), () => ({
+      default: {
+        output: "desktop",
+      },
+    }));
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      ["i", "@tauri-apps/cli"],
+      prodOptions,
+    ]);
+    expect(mockSpawnSync.mock.calls[2]).toEqual([
+      "bunx",
+      [
+        "tauri",
+        "init",
+        "-A",
+        "test",
+        "-W",
+        "test",
+        "-D",
+        "../out",
+        "--dev-path",
+        "http://localhost:3000",
+        "--before-dev-command",
+        "bun dev -- -s",
+        "--before-build-command",
+        "bun run build -- -s",
+      ],
+      prodOptions,
+    ]);
+
+    expect(mockSpawnSync.mock.calls[3]).toEqual([
+      "bunx",
+      ["tauri", "build"],
+      prodOptions,
+    ]);
+  });
+
+  it('should skip desktop "brisa build" command', async () => {
+    process.argv = ["bun", "brisa", "build", "--skip-desktop"];
+
+    mock.module(path.join(FIXTURES, "brisa.config.ts"), () => ({
+      default: {
+        output: "desktop",
+      },
+    }));
+
+    await main();
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [path.join(import.meta.dir, "out", "cli", "build.js"), "PROD"],
+      prodOptions,
+    ]);
+  });
+
+  it('should execute "brisa start" command with default options', async () => {
+    process.argv = ["bun", "brisa", "start"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(2); // bun --version
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "3000",
+        "PROD",
+      ],
+      prodOptions,
+    ]);
+  });
+
+  it('should execute "brisa start --help" command', async () => {
+    process.argv = ["bun", "brisa", "start", "--help"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(1); // bun --version
+    expect(mockLog.mock.calls).toEqual([
+      ["Usage: brisa start [options]"],
+      ["Options:"],
+      [" -p, --port    Specify port"],
+      [" --help        Show help"],
+    ]);
+
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+
+  it('should execute "brisa start" command with custom port', async () => {
+    process.argv = ["bun", "brisa", "start", "--port", "5000"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(2); // bun --version
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      "bun",
+      [
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "5000",
+        "PROD",
+      ],
+      prodOptions,
+    ]);
+  });
+
+  it("should execute .bun/bin/bun when the bun command is not found", async () => {
+    mockSpawnSync = spyOn(cp, "spawnSync").mockImplementation(
+      () => ({ status: 1 }) as any,
+    );
+
+    process.argv = ["bun", "brisa", "start"];
+
+    await main();
+
+    expect(mockSpawnSync).toHaveBeenCalledTimes(2); // bun --version
+
+    expect(mockSpawnSync.mock.calls[0]).toEqual([
+      "bun",
+      ["--version"],
+      { stdio: "ignore" },
+    ]);
+
+    expect(mockSpawnSync.mock.calls[1]).toEqual([
+      path.join(process.env.HOME!, ".bun", "bin", "bun"),
+      [
+        path.join(import.meta.dir, "out", "cli", "serve", "index.js"),
+        "3000",
+        "PROD",
+      ],
+      prodOptions,
+    ]);
+  });
+});
