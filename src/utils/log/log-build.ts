f56e7e45410585cbe1b1c522ed162ae2c72648dd
Aral Roca <aral-rg@hotmail.com>
2024-01-25 19:30:57 +0100

    feat: implement integrations type

--- src/utils/log/log-build.ts
+++ src/utils/log/log-build.ts
@@ -57,18 +57,3 @@ export function logError(messages: string[], footer?: string) {
 export function logWarning(messages: string[], footer?: string) {
   return log("Warning")(messages, footer);
 }
-
-export const greenLog = (text: string) =>
-  Bun.enableANSIColors ? `\x1b[32m${text}\x1b[0m` : text;
-
-export const yellowLog = (text: string) =>
-  Bun.enableANSIColors ? `\x1b[33m${text}\x1b[0m` : text;
-
-export const redLog = (text: string) =>
-  Bun.enableANSIColors ? `\x1b[31m${text}\x1b[0m` : text;
-
-export const blueLog = (text: string) =>
-  Bun.enableANSIColors ? `\x1b[34m${text}\x1b[0m` : text;
-
-export const cyanLog = (text: string) =>
-  Bun.enableANSIColors ? `\x1b[36m${text}\x1b[0m` : text;
