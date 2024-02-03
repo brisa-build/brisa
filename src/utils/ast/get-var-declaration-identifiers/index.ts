075d9f81df14754993aa68d5065f7bafc029ef91
Aral Roca <aral-rg@hotmail.com>
2024-02-03 18:28:44 +0100

    test(compiler-actions): fix more test cases

--- src/utils/ast/get-var-declaration-identifiers/index.ts
+++ src/utils/ast/get-var-declaration-identifiers/index.ts
@@ -6,9 +6,16 @@ export default function getVarDeclarationIdentifiers(node: ESTree.Node) {
   if (!node) return identifiers;
 
   JSON.stringify(node, (k, v) => {
-    if (v?.type === "VariableDeclarator") {
+    if (v?.type !== "VariableDeclarator") return v;
+
+    if (v.id.type === "ObjectPattern") {
+      for (const property of v.id.properties) {
+        identifiers.add(property.key.name);
+      }
+    } else if (v.id.type === "Identifier") {
       identifiers.add(v.id.name);
     }
+
     return v;
   });
 
