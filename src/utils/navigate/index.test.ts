32fc8c6e363bbf334f574b50c46c1a3172e6d77c
Aral Roca <aral-rg@hotmail.com>
2024-02-05 21:24:19 +0100

    feat: support navigate function in client-side

--- src/utils/navigate/index.test.ts
+++ src/utils/navigate/index.test.ts
@@ -1,10 +1,23 @@
-import { describe, expect, it } from "bun:test";
+import { describe, expect, it, spyOn } from "bun:test";
 import navigate from ".";
+import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
 describe("utils", () => {
   describe("navigate", () => {
     it("should throw a navigation", () => {
       expect(() => navigate("/some")).toThrow("/some");
+    });
+
+    it("should throw a navigation in client-side and change the location.assign", () => {
+      GlobalRegistrator.register();
+      const mockEventListener = spyOn(window, "addEventListener");
+      const mockLocationAssign = spyOn(location, "assign");
+      expect(() => navigate("https://test.com/some")).toThrow(
+        "https://test.com/some",
+      );
+      expect(mockLocationAssign).toHaveBeenCalledWith("https://test.com/some");
+      expect(mockEventListener).toHaveBeenCalled();
+      GlobalRegistrator.unregister();
     });
   });
 });
