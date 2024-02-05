24e383a9f7c344586b97e7c163500e3f250da3cd
Aral Roca <aral-rg@hotmail.com>
2024-02-05 12:16:56 +0100

    feat: add navigate throwable

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,6 +2,14 @@ import createPortal from "@/utils/create-portal";
 import dangerHTML from "@/utils/danger-html";
 import createContext from "@/utils/create-context";
 import notFound from "@/utils/not-found";
+import navigate from "@/utils/navigate";
 import translateCore from "@/utils/translate-core";
 
-export { createPortal, dangerHTML, createContext, notFound, translateCore };
+export {
+  createPortal,
+  dangerHTML,
+  createContext,
+  notFound,
+  navigate,
+  translateCore,
+};
