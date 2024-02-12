2fa5ea3a163752902e7060cda191c3bdeb9db55a
Aral Roca <aral-rg@hotmail.com>
2024-02-12 21:04:33 +0100

    feat(rpc): add diff stream structure

--- src/utils/inject-action-rpc/resolve-rpc.ts
+++ src/utils/inject-action-rpc/resolve-rpc.ts
@@ -1,23 +1,60 @@
+type NodeWithChildNodes = {
+  childNodes: Node[];
+  firstChild: Node | null;
+  nextSibling: Node | null;
+  parentNode: Node | null;
+  nodeName: string;
+};
+
+type Node = NodeWithChildNodes | null;
+
+const START_CHUNK_SELECTOR = "start-next-chunk";
+const decoder = new TextDecoder();
+
 async function resolveRPC(res: Response) {
   const urlToNavigate = res.headers.get("X-Navigate");
 
   return urlToNavigate
     ? (window.location.href = urlToNavigate)
-    : updateDOMFromHTMLStreamReader(res.body!.getReader());
+    : diffStream(res.body!.getReader());
 }
 
-async function updateDOMFromHTMLStreamReader(
-  reader: ReadableStreamDefaultReader<Uint8Array>,
+async function diffStream(
+  streamReader: ReadableStreamDefaultReader<Uint8Array>,
+  wrapper = document.createElement("html"),
+  text = "",
 ) {
-  while (true) {
-    const { done, value } = await reader.read();
+  const { done, value } = await streamReader.read();
 
-    if (done) break;
+  if (done) return;
 
-    const text = new TextDecoder().decode(value);
+  // Append the new chunk to the text with a marker
+  text = `${text.replace(
+    START_CHUNK_SELECTOR,
+    "",
+  )}<${START_CHUNK_SELECTOR} />${decoder.decode(value)}`;
 
-    // TODO: Implement the logic to update the DOM from the HTML stream
+  wrapper.innerHTML = text;
+
+  for (
+    let node = nextNode(wrapper.querySelector(START_CHUNK_SELECTOR) as Node);
+    node;
+    node = nextNode(node)
+  ) {
+    console.log(node.nodeName);
+    // TODO: Implement diffing algorithm
   }
+
+  return await diffStream(streamReader, wrapper, text);
+}
+
+/**
+ * Get the next node in the tree (depth-first search)
+ */
+function nextNode(node: Node, deeperDone = 0): Node | undefined {
+  if (!node) return;
+  if (node.childNodes.length && !deeperDone) return node.firstChild;
+  return node.nextSibling ?? nextNode(node.parentNode, 1);
 }
 
 window._rpc = resolveRPC;
