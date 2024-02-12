4dca0a5ae121acf5f1f42e9543e105a94e11d986
Aral Roca <aral-rg@hotmail.com>
2024-02-13 00:22:41 +0100

    feat(rpc): parse with declarative shadow roots

--- src/utils/inject-action-rpc/resolve-rpc.ts
+++ src/utils/inject-action-rpc/resolve-rpc.ts
@@ -10,6 +10,7 @@ type Node = NodeWithChildNodes | null;
 
 const START_CHUNK_SELECTOR = "start-next-chunk";
 const decoder = new TextDecoder();
+const parser = new DOMParser();
 
 async function resolveRPC(res: Response) {
   const urlToNavigate = res.headers.get("X-Navigate");
@@ -19,12 +20,11 @@ async function resolveRPC(res: Response) {
     return;
   }
 
-  updateDOMReactively(res.body!.getReader());
+  await updateDOMReactively(res.body!.getReader());
 }
 
 async function updateDOMReactively(
   streamReader: ReadableStreamDefaultReader<Uint8Array>,
-  wrapper = document.createElement("html"),
   text = "",
 ) {
   const { done, value } = await streamReader.read();
@@ -39,35 +39,41 @@ async function updateDOMReactively(
     "",
   )}<${START_CHUNK_SELECTOR} />${decoder.decode(value)}`;
 
+  // Parse the text into a DOM document with declarative shadow roots
+  // @ts-ignore
+  const doc = parser.parseFromString(text, "text/html", {
+    includeShadowRoots: true,
+  });
+
   // Replace the wrapper with the text of all the chunks that have
-  // arrived to don't lose the previous element because some chunks
+  // arrived to don't lose the previous node because some chunks
   // are only end tags, opening tags, or text and we need to keep
   // the full context
-  wrapper.innerHTML = text;
+  // wrapper.innerHTML = text;
 
-  // Iterate over the chunk elements to diff
+  // Iterate over the chunk nodes to diff
   for (
-    let element = nextElement(wrapper.querySelector(START_CHUNK_SELECTOR));
-    element;
-    element = nextElement(element)
+    let node = nextNode(doc.querySelector(START_CHUNK_SELECTOR) as Node);
+    node;
+    node = nextNode(node)
   ) {
-    console.log(element.nodeName);
+    console.log(node.nodeName);
     // TODO: Implement diffing algorithm
   }
 
   // Continue reading the stream, doing the diff of the next chunk
-  return await updateDOMReactively(streamReader, wrapper, text);
+  return await updateDOMReactively(streamReader, text);
 }
 
 /**
- * Get the next element in the tree. A difference here between the normal
+ * Get the next node in the tree. A difference here between the normal
  * Virtual DOM diffing algorith is that we need to change the breadth-first
  * to depth-first search in order to work with the streamed HTML.
  */
-function nextElement(element: Element | null, deeperDone = 0): Element | null {
-  if (!element) return null;
-  if (element.children.length && !deeperDone) return element.firstElementChild;
-  return element.nextElementSibling ?? nextElement(element.parentElement, 1);
+function nextNode(node: Node | null, deeperDone?: Boolean): Node | null {
+  if (!node) return null;
+  if (node.childNodes.length && !deeperDone) return node.firstChild;
+  return node.nextSibling ?? nextNode(node.parentNode, true);
 }
 
 window._rpc = resolveRPC;
