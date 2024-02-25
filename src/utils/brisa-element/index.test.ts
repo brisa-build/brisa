ffa3b73420f8923707f5b423903e52ec7a1bc282
Aral Roca <aral-rg@hotmail.com>
2024-02-25 19:35:52 +0100

    chore: update bun version

--- src/utils/brisa-element/index.test.ts
+++ src/utils/brisa-element/index.test.ts
@@ -1211,7 +1211,7 @@ describe("utils", () => {
       expect(div.namespaceURI).toBe("http://www.w3.org/1999/xhtml");
 
       expect(testSVG?.shadowRoot?.innerHTML).toBe(
-        '<svg width="12cm" height="12cm"><foreignobject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml">test</div></foreignobject></svg>',
+        '<svg width="12cm" height="12cm"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml">test</div></foreignObject></svg>',
       );
     });
 
--- src/utils/client-build-plugin/integration.test.ts
+++ src/utils/client-build-plugin/integration.test.ts
@@ -1464,7 +1464,7 @@ describe("integration", () => {
       expect(div.namespaceURI).toBe("http://www.w3.org/1999/xhtml");
 
       expect(testSVG?.shadowRoot?.innerHTML).toBe(
-        '<svg width="12cm" height="12cm"><foreignobject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml">test</div></foreignobject></svg>',
+        '<svg width="12cm" height="12cm"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml">test</div></foreignObject></svg>',
       );
     });
 
