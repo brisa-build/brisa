14a141c0764e4163cdbfe34f205a53d0ff88ecfa
Aral Roca <aral-rg@hotmail.com>
2024-03-16 22:42:02 +0100

    test: add workaround to test

--- src/utils/response-action/index.test.ts
+++ src/utils/response-action/index.test.ts
@@ -9,7 +9,7 @@ import {
 } from "bun:test";
 import path from "node:path";
 import extendRequestContext from "@/utils/extend-request-context";
-import responseAction from ".";
+import { responseAction } from "./test-mock";
 import { getConstants } from "@/constants";
 import { ENCRYPT_NONTEXT_PREFIX, encrypt } from "@/utils/crypto";
 
