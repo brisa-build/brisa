7cd2d80c1c15d45eccaf5db774fbdfcc32e5d61f
Aral Roca <aral-rg@hotmail.com>
2024-03-21 14:32:16 +0100

    fix(build): fix actions in layout

---
+++ src/__fixtures__/with-action-in-layout/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }: { children: JSX.Element }) {
+  return (
+    <html>
+      <head>
+        <title id="title">CUSTOM LAYOUT</title>
+      </head>
+      <body onClick={() => console.log("Hello world")}>{children}</body>
+    </html>
+  );
+}
