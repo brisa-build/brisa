b61d0962071c233139be8cccec9463245ca3abc2
Aral Roca <aral-rg@hotmail.com>
2024-03-21 12:13:33 +0100

    refactor: isolate test about web component in layout

--- src/__fixtures__/with-complex-layout/out/pages/index.js
+++
@@ -1,2 +0,0 @@
-// @bun
-var b=function(i,_){return{type:i,props:_}};function u(){return b("div",{children:"hello world"},void 0,!1,void 0,this)}var a=(i)=>i.children;a.__isFragment=!0;export{u as default};
