fd83a08f17233023c81a1f7da91b48299f30d107
Aral Roca <aral-rg@hotmail.com>
2024-03-21 15:23:41 +0100

    fix(build): fix i18n client keys in layout

---
+++ src/utils/compile-files/__fixtures__/with-i18nkeys-in-layout/i18n.tsx
@@ -0,0 +1,14 @@
+export default {
+  defaultLocale: "en",
+  locales: ["en", "fr"],
+  messages: {
+    en: {
+      "server-key": "Hello server!",
+      "client-key": "Hello client!",
+    },
+    fr: {
+      "server-key": "Bonjour serveur !",
+      "client-key": "Bonjour client !",
+    },
+  },
+};
