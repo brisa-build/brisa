97fcad93c1883018be10b9146cc4e4daaf1dd9fb
Aral Roca <aral-rg@hotmail.com>
2024-03-21 12:13:33 +0100

    refactor: isolate test about web component in layout

--- src/__fixtures__/with-complex-layout/out/_brisa/types.ts
+++
@@ -1,3 +0,0 @@
-export interface IntrinsicCustomElements {
-  'layout-web-component': JSX.WebComponentAttributes<typeof import("/Users/aralroca/Documents/repos/brisa/src/__fixtures__/with-complex-layout/web-components/layout-web-component.tsx").default>;
-}
