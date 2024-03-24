fdedf2fb3df9481ed070ac71b2acff5522265acb
Aral Roca <aral-rg@hotmail.com>
2024-03-24 08:48:00 +0100

    fix: allow request without extension on renderToReadableStream

--- src/utils/render-to-string/index.tsx
+++ src/utils/render-to-string/index.tsx
@@ -1,11 +1,8 @@
 import renderToReadableStream from "@/utils/render-to-readable-stream";
-import extendRequestContext from "@/utils/extend-request-context";
 
 export default async function renderToString(
   element: JSX.Element,
-  request = extendRequestContext({
-    originalRequest: new Request("http://localhost"),
-  }),
+  request = new Request("http://localhost"),
 ): Promise<string> {
   return await Bun.readableStreamToText(
     renderToReadableStream(element, { request, log: false } as any),
