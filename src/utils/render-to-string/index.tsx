2ba25f30d16456e6e6503bc92ccd31dba2725112
Aral Roca <aral-rg@hotmail.com>
2024-03-24 07:58:07 +0100

    fix: fix default req in renderToString

--- src/utils/render-to-string/index.tsx
+++ src/utils/render-to-string/index.tsx
@@ -1,8 +1,9 @@
 import renderToReadableStream from "@/utils/render-to-readable-stream";
+import extendRequestContext from "@/utils/extend-request-context";
 
 export default async function renderToString(
   element: JSX.Element,
-  request = new Request("http://localhost"),
+  request = extendRequestContext({ originalRequest: new Request("http://localhost") }),
 ): Promise<string> {
   return await Bun.readableStreamToText(
     renderToReadableStream(element, { request, log: false } as any),
