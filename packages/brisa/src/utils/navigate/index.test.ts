d7e6149c8c801600fe580fe5bcd508bd3c9adfeb
Aral Roca <aral-rg@hotmail.com>
2024-04-13 18:37:57 +0200

    feat: support basePath in brisa assets

--- packages/brisa/src/utils/navigate/index.test.ts
+++ packages/brisa/src/utils/navigate/index.test.ts
@@ -6,39 +6,40 @@ const BASE_PATHS = ["", "/some", "/some/other"];
 
 describe("utils", () => {
   describe.each(BASE_PATHS)("navigate %s", (basePath) => {
-
     describe("server side", () => {
       it("should throw a navigation", () => {
         expect(() => navigate("/some")).toThrow(`${basePath}/some`);
       });
       it("should throw a navigation without adding the basePath to the external URL", () => {
-        expect(() => navigate("https://test.com/some")).toThrow(`https://test.com/some`);
+        expect(() => navigate("https://test.com/some")).toThrow(
+          `https://test.com/some`,
+        );
       });
-    })
-    
-    describe('client side', () => {
-      beforeEach(() =>  GlobalRegistrator.register())
-      afterEach(() =>  GlobalRegistrator.unregister())
+    });
+
+    describe("client side", () => {
+      beforeEach(() => GlobalRegistrator.register());
+      afterEach(() => GlobalRegistrator.unregister());
 
       it("should throw a navigation in client-side and change the location.assign", () => {
         const mockEventListener = spyOn(window, "addEventListener");
         const mockLocationAssign = spyOn(location, "assign");
-        expect(() => navigate("/some")).toThrow(
-          `${basePath}/some`,
-        );
+        expect(() => navigate("/some")).toThrow(`${basePath}/some`);
         expect(mockLocationAssign).toHaveBeenCalledWith(`${basePath}/some`);
         expect(mockEventListener).toHaveBeenCalled();
       });
-  
+
       it("should throw a navigation in client-side and change the location.assign without adding basePath to the external URL", () => {
         const mockEventListener = spyOn(window, "addEventListener");
         const mockLocationAssign = spyOn(location, "assign");
         expect(() => navigate("https://test.com/some")).toThrow(
           "https://test.com/some",
         );
-        expect(mockLocationAssign).toHaveBeenCalledWith("https://test.com/some");
+        expect(mockLocationAssign).toHaveBeenCalledWith(
+          "https://test.com/some",
+        );
         expect(mockEventListener).toHaveBeenCalled();
       });
-    })
+    });
   });
 });
