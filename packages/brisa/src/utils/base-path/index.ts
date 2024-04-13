aa5fe35880b5d96326a90bb462785bb15f041570
Aral Roca <aral-rg@hotmail.com>
2024-04-13 10:36:40 +0200

    refactor: improve base path utils

--- packages/brisa/src/utils/base-path/index.ts
+++ packages/brisa/src/utils/base-path/index.ts
@@ -3,35 +3,26 @@ import { getConstants } from "@/constants";
 export function addBasePathToStringURL(url: string) {
   const { CONFIG } = getConstants();
   const basePath = CONFIG.basePath || "";
-  let finalUrl;
 
-  if (URL.canParse(url)) {
-    const urlInstance = new URL(url);
-    urlInstance.pathname = basePath + urlInstance.pathname;
-    finalUrl = urlInstance.toString();
-  } else {
-    finalUrl = basePath + url;
-  }
-
-  // Remove trailing slash if the original url doesn't have it
-  if (!url.endsWith("/") && finalUrl.endsWith("/")) {
-    return finalUrl.slice(0, -1);
-  }
-
-  return finalUrl;
+  return processURL(url, (pathname) => `${basePath}${pathname}`);
 }
 
 export function removeBasePathFromStringURL(url: string) {
   const { CONFIG } = getConstants();
   const basePath = CONFIG.basePath || "";
+
+  return processURL(url, (pathname) => pathname.replace(basePath, ""));
+}
+
+function processURL(url: string, operate: (pathname: string) => string) {
   let finalUrl;
 
   if (URL.canParse(url)) {
     const urlInstance = new URL(url);
-    urlInstance.pathname = urlInstance.pathname.replace(basePath, "");
+    urlInstance.pathname = operate(urlInstance.pathname);
     finalUrl = urlInstance.toString();
   } else {
-    finalUrl = url.replace(basePath, "");
+    finalUrl = operate(url);
   }
 
   // Remove trailing slash if the original url doesn't have it
