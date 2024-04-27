787db0eb786ce71aca4fde976a24db9db8f47ee3
Aral Roca <aral-rg@hotmail.com>
2024-04-27 17:42:49 +0200

    refactor(rpc): use ternary to reduce bundle size

--- packages/brisa/src/utils/get-client-code-in-page/index.test.ts
+++ packages/brisa/src/utils/get-client-code-in-page/index.test.ts
@@ -21,7 +21,7 @@ const i18nCode = 3072;
 const brisaSize = 5650; // TODO: Reduce this size
 const webComponents = 684;
 const unsuspenseSize = 217;
-const rpcSize = 2250; // TODO: Reduce this size
+const rpcSize = 2240; // TODO: Reduce this size
 const lazyRPCSize = 3509; // TODO: Reduce this size
 // lazyRPC is loaded after user interaction (action, link),
 // so it's not included in the initial size
