1ac3d38d8974f1332e116a64185bc363def57cb2
Aral Roca <aral-rg@hotmail.com>
2024-04-28 21:08:22 +0200

    fix loadScripts with transition

--- packages/brisa/src/utils/rpc/load-scripts/index.test.ts
+++ packages/brisa/src/utils/rpc/load-scripts/index.test.ts
@@ -9,44 +9,76 @@ import { describe, expect, it, beforeEach, afterEach, spyOn } from "bun:test";
 let mockLog: ReturnType<typeof spyOn>;
 
 describe("utils", () => {
-  beforeEach(() => {
-    GlobalRegistrator.register();
-    mockLog = spyOn(console, "log");
-  });
-  afterEach(() => {
-    mockLog.mockRestore();
-    GlobalRegistrator.unregister();
-    scripts.clear();
-  });
-
-  function createScript(src: string, content?: string) {
-    document.head.innerHTML = content
-      ? `<script>${content}</script>`
-      : `<script src=${src}></script>`;
-    registerCurrentScripts();
-    const script = document.createElement("script");
-    if (src) script.src = src;
-    if (content) script.innerHTML = content;
-    return script;
-  }
-
   describe("load-scripts", () => {
+    beforeEach(() => {
+      GlobalRegistrator.register();
+      mockLog = spyOn(console, "log");
+    });
+    afterEach(() => {
+      mockLog.mockRestore();
+      GlobalRegistrator.unregister();
+      scripts.clear();
+    });
+
+    function createScript(src: string, content?: string, id = "") {
+      document.head.innerHTML = content
+        ? `<script id="${id}">${content}</script>`
+        : `<script src=${src}></script>`;
+      const script = document.createElement("script");
+      if (src) script.src = src;
+      if (id) script.id = id;
+      if (content) script.innerHTML = content;
+      return script;
+    }
+
     it("should load script with 'src' attribute", async () => {
       const src = `data:text/javascript;base64,${btoa(
         `console.log('hello from fetch')`,
       )}`;
       const script = createScript(src);
-      loadScripts(script);
+
+      await loadScripts(script);
+      registerCurrentScripts();
+
       expect(scripts.size).toBe(1);
       expect(scripts.has("data:text/javascript")).toBeTrue();
       expect(mockLog).toHaveBeenCalledWith("hello from fetch");
     });
 
-    it("should load script with content", () => {
+    it("should load script with content without register", async () => {
       const content = "console.log('hello')";
       const script = createScript("", content);
-      expect(scripts.size).toBe(0); // no src
-      loadScripts(script);
+
+      await loadScripts(script);
+      registerCurrentScripts();
+
+      expect(scripts.size).toBe(0); // no src neither id
+      expect(mockLog).toHaveBeenCalledWith("hello");
+    });
+
+    it("should load script with content and register with the id", async () => {
+      const content = "console.log('hello')";
+      const script = createScript("", content, "some-id");
+
+      await loadScripts(script);
+      registerCurrentScripts();
+
+      expect(scripts.size).toBe(1);
+      expect(mockLog).toHaveBeenCalledWith("hello");
+    });
+
+    // This is to avoid cases navigating before wait the suspense to be resolved
+    // and in the next page there is the same suspense component, it's better to
+    // execute the unsuspense script always, after execute the suspense script, it's
+    // removed from the DOM
+    it("should register to scripts if is a unsuspense script", async () => {
+      const content = "console.log('hello')";
+      const script = createScript("", content, "R:1");
+
+      await loadScripts(script);
+      registerCurrentScripts();
+
+      expect(scripts.size).toBe(0);
       expect(mockLog).toHaveBeenCalledWith("hello");
     });
   });
--- packages/brisa/src/utils/rpc/load-scripts/index.ts
+++ packages/brisa/src/utils/rpc/load-scripts/index.ts
@@ -2,21 +2,32 @@ export const scripts = new Set();
 
 export function registerCurrentScripts() {
   for (let script of document.scripts) {
-    if (script.hasAttribute("src")) scripts.add(script.getAttribute("src"));
+    const hasValidID = script.id && !/R:\d+/.test(script.id);
+    if (hasValidID || script.hasAttribute("src")) {
+      scripts.add(script.id || script.getAttribute("src"));
+    }
   }
 }
 
 // Load new scripts and manage "unsuspense" scripts
-export function loadScripts(node: Node) {
-  if (node.nodeName === "SCRIPT") {
-    const src = (node as HTMLScriptElement).getAttribute("src");
-    if (!scripts.has(src)) {
-      const script = document.createElement("script");
-      if (src) script.src = src;
-      script.innerHTML = (node as HTMLScriptElement).innerHTML;
-      script.onload = script.onerror = () => script.remove();
-      document.head.appendChild(script);
-      if (!src) script.remove();
-    }
+export async function loadScripts(node: Node) {
+  if (node.nodeName !== "SCRIPT") return;
+
+  const src = (node as HTMLScriptElement).getAttribute("src");
+
+  if (scripts.has(src) || scripts.has((node as HTMLScriptElement).id)) {
+    return;
   }
+
+  if (window.lastDiffTransition) await window.lastDiffTransition.finished;
+
+  const script = document.createElement("script");
+
+  if (src) script.src = src;
+
+  script.innerHTML = (node as HTMLScriptElement).innerHTML;
+  script.onload = script.onerror = () => script.remove();
+  document.head.appendChild(script);
+
+  if (!src) script.remove();
 }
