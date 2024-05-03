4c46a57ad226664ef7e910ffdec93a36e5eaf48c
Aral Roca <aral-rg@hotmail.com>
2024-05-03 18:22:52 +0200

    fix(prerender): modify generateStaticExport to return Map of arrays

--- packages/brisa/src/utils/compile-files/index.ts
+++ packages/brisa/src/utils/compile-files/index.ts
@@ -13,7 +13,7 @@ import serverComponentPlugin from "@/utils/server-component-plugin";
 import createContextPlugin from "@/utils/create-context/create-context-plugin";
 import getI18nClientMessages from "@/utils/get-i18n-client-messages";
 import compileActions from "@/utils/compile-actions";
-import generateStaticExport from "@/utils/generate-static-export";
+import { generateStaticExport } from "@/cli/build-utils";
 
 export default async function compileFiles() {
   const {
