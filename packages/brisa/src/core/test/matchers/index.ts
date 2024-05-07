2c2f78eb3919bc83d78b0d3836f0e440982db03b
Aral Roca <aral-rg@hotmail.com>
2024-05-07 22:05:06 +0200

    feat(testing): add toBeChecked matcher

---
+++ packages/brisa/src/core/test/matchers/index.ts
@@ -0,0 +1,41 @@
+function toBeChecked(received: unknown) {
+  if (received instanceof HTMLInputElement === false) {
+    throw new Error(
+      "Invalid usage of toBeChecked(received). The argument received should be an HTMLInputElement",
+    );
+  }
+
+  return {
+    pass: received.checked,
+    message: () => "expected input element to be checked",
+  };
+}
+
+/**
+ * 
+ * TODO:
+- `toHaveAttribute`: Verifies whether a specified attribute exists within the target element.
+- `toHaveTagName`: Checks if the target element has a specific HTML tag.
+- `toHaveRenderedText`: Ensures that the target element renders the expected text content.
+- `toHaveBeenRenderedTimes`: Checks the number of times the target element has been rendered.
+- `toContainRenderedText`: Verifies if the rendered text content contains a specific string.
+- `toHaveStyle`: Validates the styling properties of the target element.
+- `toHaveClass`: Checks for the presence of a specified CSS class within the target element.
+- `toHaveValue`: Verifies the current value of form elements such as input fields.
+- `toHaveFocus`: Indicates whether the target element currently has focus.
+- `toBeSuspenseMode`: Checks if the target component is in suspense mode.
+- `toFinishSuspenseMode`: Verifies if the suspense mode of the target component has finished.
+- `toBeErrorMode`: Indicates whether the target component is in error mode.
+- `toBeVisible`: Checks if the target element is visible within the DOM.
+- `toBeEnabled`: Verifies that the target element is enabled and interactive.
+- `toBeDisabled`: Indicates whether the target element is disabled and non-interactive.
+- `toBeSelected`: Indicates whether the target element is selected.
+- `toBeRequired`: Indicates whether the target element is required.
+- `toBeInvalid`: Indicates whether the target element is invalid.
+- `toBeValid`: Checks if the target element is valid.
+- `toBeHidden`: Verifies if the target element is hidden.
+ */
+
+export default {
+  toBeChecked,
+};
