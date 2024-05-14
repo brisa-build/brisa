a840fb759cf343f17ec80d72cd07db3b6f6469ca
Aral Roca <aral-rg@hotmail.com>
2024-05-14 22:48:13 +0200

    docs(testing): improve types

--- packages/brisa/src/types/test.d.ts
+++ packages/brisa/src/types/test.d.ts
@@ -405,3 +405,23 @@ type userEvent = {
  * - [Brisa docs](https://brisa.build/building-your-application/testing/test-api#userevent)
  */
 export const userEvent: userEvent;
+
+/**
+ * cleanup - Brisa Test API
+ * 
+ * Cleanup the test environment cleaning up the DOM and other resources.
+ * 
+ * Example:
+ * 
+ * ```tsx
+ * import { cleanup } from "brisa";
+ * import { afterEach } from "bun:test";
+ * 
+ * afterEach(() => {
+ *  cleanup();
+ * });
+ * ```
+ * 
+ * - [Brisa docs](https://brisa.build/building-your-application/testing/test-api#cleanup)
+ */
+export function cleanup(): void;
