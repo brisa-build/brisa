d28de99dac433bda22438cb1342f25749127b97e
Aral Roca <aral-rg@hotmail.com>
2024-05-16 17:20:08 +0200

    fix: avoid building test files

--- packages/brisa/src/utils/get-entrypoints/index.ts
+++ packages/brisa/src/utils/get-entrypoints/index.ts
@@ -1,7 +1,11 @@
 import fs from "node:fs";
 
+const DENIED_SUFFIXES_REGEX = /\.test\.(tsx|ts|js|jsx|cjs|mjs)$/;
+
 export default function getEntrypoints(dir: string) {
   if (!fs.existsSync(dir)) return [];
   const router = new Bun.FileSystemRouter({ style: "nextjs", dir });
-  return Object.values(router.routes);
+  return Object.values(router.routes).filter(
+    (route) => !route.match(DENIED_SUFFIXES_REGEX),
+  );
 }
