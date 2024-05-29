7a0a138941e5f448af0bb4166d50300673e6e138
Aral Roca <aral-rg@hotmail.com>
2024-05-29 23:02:45 +0200

    fix: fix renderInitiator and improve types

--- packages/brisa/src/public-constants.ts
+++ packages/brisa/src/public-constants.ts
@@ -1,5 +1,7 @@
+import type { RenderInitiatorType } from "@/types/server";
+
 export const RenderInitiator = {
   INITIAL_REQUEST: "INITIAL_REQUEST",
   SPA_NAVIGATION: "SPA_NAVIGATION",
   SERVER_ACTION: "SERVER_ACTION",
-} as const;
+} satisfies RenderInitiatorType;
