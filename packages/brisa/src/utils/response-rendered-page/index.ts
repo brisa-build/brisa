96db8f0891944b5784b28fb3b29d25e34dd01495
Aral Roca <aral-rg@hotmail.com>
2024-06-08 21:58:40 +0200

    refactor: separate logic to get page component with headers

--- packages/brisa/src/utils/response-rendered-page/index.ts
+++ packages/brisa/src/utils/response-rendered-page/index.ts
@@ -9,58 +9,41 @@ import importFileIfExists from "@/utils/import-file-if-exists";
 import transferStoreService from "@/utils/transfer-store-service";
 import { RenderInitiator } from "@/public-constants";
 
+type ResponseRenderedPageParams = {
+  req: RequestContext;
+  route: MatchedRoute;
+  status?: number;
+  error?: Error;
+  headers?: Record<string, string>;
+};
+
 export default async function responseRenderedPage({
   req,
   route,
   status = 200,
   error,
   headers = {},
-}: {
-  req: RequestContext;
-  route: MatchedRoute;
-  status?: number;
-  error?: Error;
-  headers?: Record<string, string>;
-}) {
+}: ResponseRenderedPageParams) {
   const { transferClientStoreToServer: transfeClientStoreToServer } =
     await transferStoreService(req);
-  const { HEADERS, BUILD_DIR } = getConstants();
-  const middlewareModule = await importFileIfExists("middleware", BUILD_DIR);
-  const { Page, module, layoutModule } = await processPageRoute(route, error);
+  const { PageComponent, pageModule, pageHeaders } =
+    await getPageComponentWithHeaders({ req, route, error, status, headers });
 
   // Avoid to transfer again if comes from a rerender from an action
   if (req.renderInitiator !== RenderInitiator.SERVER_ACTION) {
     transfeClientStoreToServer();
   }
 
-  const middlewareResponseHeaders =
-    middlewareModule?.responseHeaders?.(req, status) ?? {};
-
-  const layoutResponseHeaders =
-    layoutModule?.responseHeaders?.(req, status) ?? {};
-
-  const pageResponseHeaders =
-    (await module.responseHeaders?.(req, status)) ?? {};
-
   const fileStream = getPrerenderedPage(route);
   const htmlStream =
     fileStream ??
-    renderToReadableStream(Page(), {
+    renderToReadableStream(PageComponent(), {
       request: req,
-      head: module.Head,
+      head: pageModule.Head,
     });
 
   const responseOptions = {
-    headers: {
-      "cache-control": HEADERS.CACHE_CONTROL,
-      ...middlewareResponseHeaders,
-      ...layoutResponseHeaders,
-      ...pageResponseHeaders,
-      ...headers,
-      "transfer-encoding": "chunked",
-      vary: "Accept-Encoding",
-      "content-type": "text/html; charset=utf-8",
-    },
+    headers: pageHeaders,
     status,
   };
 
@@ -77,4 +60,39 @@ function getPrerenderedPage(route: MatchedRoute) {
   );
 
   return fs.existsSync(filePath) ? Bun.file(filePath).stream() : null;
+}
+
+export async function getPageComponentWithHeaders({
+  req,
+  route,
+  error,
+  status = 200,
+  headers,
+}: ResponseRenderedPageParams) {
+  const { HEADERS, BUILD_DIR } = getConstants();
+  const middlewareModule = await importFileIfExists("middleware", BUILD_DIR);
+  const { Page, module, layoutModule } = await processPageRoute(route, error);
+  const middlewareResponseHeaders =
+    middlewareModule?.responseHeaders?.(req, status) ?? {};
+
+  const layoutResponseHeaders =
+    layoutModule?.responseHeaders?.(req, status) ?? {};
+
+  const pageResponseHeaders =
+    (await module.responseHeaders?.(req, status)) ?? {};
+
+  return {
+    PageComponent: Page,
+    pageModule: module,
+    pageHeaders: {
+      "cache-control": HEADERS.CACHE_CONTROL,
+      ...middlewareResponseHeaders,
+      ...layoutResponseHeaders,
+      ...pageResponseHeaders,
+      ...headers,
+      "transfer-encoding": "chunked",
+      vary: "Accept-Encoding",
+      "content-type": "text/html; charset=utf-8",
+    },
+  };
 }
