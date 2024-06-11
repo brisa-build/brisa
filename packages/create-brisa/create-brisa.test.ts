2dbf4de12a1a0ea8aee22cfd1095fa9426ee0556
Aral Roca <aral-rg@hotmail.com>
2024-06-12 00:51:09 +0200

    fix #226: display error message when the folder already exists

--- packages/create-brisa/create-brisa.test.ts
+++ packages/create-brisa/create-brisa.test.ts
@@ -1,7 +1,9 @@
-import { describe, it, expect, beforeEach, afterEach, mock } from "bun:test";
+import { describe, it, expect, afterEach } from "bun:test";
 import { $ } from "bun";
 import { join } from "node:path";
+import fs from "node:fs";
 
+const CREATE_BRISA_PATH = join(import.meta.dir, "create-brisa.cjs");
 const EXPECTED_INNER_FILES = [
   "bun.lockb",
   "node_modules",
@@ -19,21 +21,23 @@ describe("create-brisa", () => {
 
   it("should create brisa correctly with the name of the project as 'out'", async () => {
     const projectName = "out";
-    await $`echo "${projectName}" | bun run ${join(
-      import.meta.dir,
-      "create-brisa.cjs",
-    )}`;
+    await $`echo "${projectName}" | bun run ${CREATE_BRISA_PATH}`;
     expect(await $`ls ${projectName}`.text()).toBe(
       EXPECTED_INNER_FILES.join("\n") + "\n",
     );
   });
 
+  it("should exit and display an error if the folder exists", async () => {
+    fs.mkdirSync("out");
+    const projectName = "out/foo";
+    const res = await $`echo "${projectName}" | bun run ${CREATE_BRISA_PATH}`;
+
+    expect(res.stderr.toString()).toBe("Error: out folder already exists\n");
+  });
+
   it("should create brisa correctly with multi-folder with the name 'out/@foo/bar/baz'", async () => {
-    const projectName = "out/@foo/bar/baz";
-    await $`echo "${projectName}" | bun run ${join(
-      import.meta.dir,
-      "create-brisa.cjs",
-    )}`;
+    const projectName = join("out", "@foo", "bar", "baz");
+    await $`echo "${projectName}" | bun run ${CREATE_BRISA_PATH}`;
     expect(await $`ls ${projectName}`.text()).toBe(
       EXPECTED_INNER_FILES.join("\n") + "\n",
     );
