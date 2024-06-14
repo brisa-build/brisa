c7286ece67580f1b4e8dc495cffec473205e53c2
Alber Tenez <albert@zenettech.com>
2024-06-14 14:59:07 +0200

    Simplify types on ssr-web-component

--- packages/brisa/src/utils/ssr-web-component/index.tsx
+++ packages/brisa/src/utils/ssr-web-component/index.tsx
@@ -9,11 +9,11 @@ import type {
 import { getConstants } from "@/constants";
 
 export const AVOID_DECLARATIVE_SHADOW_DOM_SYMBOL = Symbol.for(
-  "AVOID_DECLARATIVE_SHADOW_DOM",
+  "AVOID_DECLARATIVE_SHADOW_DOM"
 );
 
-type Props<T extends Record<string, unknown>> = T & {
-  Component: JSXComponent<T>;
+type SSRWebComponentProps = {
+  Component: JSXComponent;
   selector: string;
   children?: JSXElement;
 };
@@ -28,11 +28,9 @@ function isPromise<T>(v: unknown): v is Promise<T> {
   );
 }
 
-export default async function SSRWebComponent<
-  T extends Record<string, unknown> = {},
->(
-  { Component, selector, children, ...props }: Props<T>,
-  { store, useContext, i18n, indicate }: RequestContext,
+export default async function SSRWebComponent(
+  { Component, selector, children, ...props }: SSRWebComponentProps,
+  { store, useContext, i18n, indicate }: RequestContext
 ): Promise<JSXNode> {
   const { WEB_CONTEXT_PLUGINS } = getConstants();
   const showContent = !store.has(AVOID_DECLARATIVE_SHADOW_DOM_SYMBOL);
@@ -56,7 +54,7 @@ export default async function SSRWebComponent<
     css: (template: TemplateStringsArray, ...values: string[]) => {
       style += String.raw(
         template,
-        ...values.map((v: unknown) => (typeof v === "function" ? v() : v)),
+        ...values.map((v: unknown) => (typeof v === "function" ? v() : v))
       );
     },
   } as unknown as RequestContext;
@@ -81,7 +79,7 @@ export default async function SSRWebComponent<
       if (Component.error) {
         const maybePromise = Component.error(
           { ...componentProps, error },
-          webContext,
+          webContext
         );
 
         content = isPromise<JSXNode>(maybePromise)
