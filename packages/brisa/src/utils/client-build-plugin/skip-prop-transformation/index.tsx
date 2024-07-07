21086a63ebeadd14fab8dd2ba7dbb02f6e798ef0
Aral Roca <aral-rg@hotmail.com>
2024-07-07 20:43:15 +0200

    fix(build): fix destructured variables

--- packages/brisa/src/utils/client-build-plugin/skip-prop-transformation/index.tsx
+++ packages/brisa/src/utils/client-build-plugin/skip-prop-transformation/index.tsx
@@ -2,19 +2,34 @@ export default function skipPropTransformation(
   propsNamesAndRenamesSet: Set<string>,
 ) {
   return function traverseA2B(this: any, key: string, value: any) {
-    if (this._skip && typeof value === "object" && value !== null) {
-      if (!value._skip) value._skip = this._skip.slice();
+    if (
+      !value._skip &&
+      this._skip &&
+      typeof value === "object" &&
+      value !== null
+    ) {
+      value._skip = this._skip.slice();
     }
 
     if (value?.type === "VariableDeclaration" && Array.isArray(this)) {
       const skipArray = value?._skip?.slice() ?? [];
 
       for (const declaration of value.declarations) {
+        // Skip variable declarations
         if (
           declaration?.id?.type === "Identifier" &&
           propsNamesAndRenamesSet.has(declaration?.id?.name)
         ) {
           skipArray.push(declaration?.id.name);
+        }
+
+        // Skip object pattern properties
+        if (declaration?.id?.type === "ObjectPattern") {
+          const names = getAllObjectPatternNamesRecursive(declaration?.id);
+
+          for (const name of names) {
+            if (propsNamesAndRenamesSet.has(name)) skipArray.push(name);
+          }
         }
       }
 
@@ -29,4 +44,21 @@ export default function skipPropTransformation(
 
     return value;
   };
+}
+
+function getAllObjectPatternNamesRecursive(
+  objectPattern: any,
+  names = new Set<string>(),
+) {
+  if (objectPattern?.type !== "ObjectPattern") return names;
+
+  for (const prop of objectPattern.properties) {
+    if (prop.type === "RestElement") {
+      getAllObjectPatternNamesRecursive(prop.argument, names);
+    } else {
+      names.add(prop.value.name);
+    }
+  }
+
+  return names;
 }
