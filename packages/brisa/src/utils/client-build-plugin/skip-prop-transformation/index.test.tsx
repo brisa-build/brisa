b3321e3d22a21a658d96d3115a71ce02d6d6773f
Aral Roca <aral-rg@hotmail.com>
2024-07-07 20:17:07 +0200

    feat: update next siblings to skip

--- packages/brisa/src/utils/client-build-plugin/skip-prop-transformation/index.test.tsx
+++ packages/brisa/src/utils/client-build-plugin/skip-prop-transformation/index.test.tsx
@@ -3,7 +3,7 @@ import getWebComponentAst from "@/utils/client-build-plugin/get-web-component-as
 import skipPropTransformation from "@/utils/client-build-plugin/skip-prop-transformation";
 import { describe, expect, it } from "bun:test";
 
-const { parseCodeToAST } = AST("tsx");
+const { parseCodeToAST, generateCodeFromAST } = AST("tsx");
 
 describe("client-build-plugin/skip-prop-transformation", () => {
   it("should skip scopes with variables with the same name as prop", () => {
@@ -17,14 +17,46 @@ describe("client-build-plugin/skip-prop-transformation", () => {
       }
     `;
     const props = new Set(["foo"]);
+    const out = applySkipTest(code, props);
+    const lines = getOutputCodeLines(out, "foo");
 
-    expect(applySkipTest(code, props)).toEqual([
-      {
-        type: "Identifier",
-        name: "foo",
-        _skip: ["foo"],
-      },
+    expect(lines).toEqual(["console.log(foo);"]);
+  });
+
+  it("should skip all the rest of the scope with variables with the same name as prop", () => {
+    const code = `
+      export default function Component({foo}) {
+        function onClick() {
+          const foo = 1;
+          console.log(foo);
+          const bar = 2;
+          console.log(bar);
+          const baz = 3;
+          console.log(baz);
+        }
+        return <div onClick={onClick}>{foo}</div>;
+      }
+    `;
+    const props = new Set(["foo", "bar", "baz"]);
+    const out = applySkipTest(code, props);
+
+    console.log(out);
+
+    expect(getOutputCodeLines(out, "foo")).toEqual([
+      "console.log(foo);",
+      "const bar = 2;",
+      "console.log(bar);",
+      "const baz = 3;",
+      "console.log(baz);",
     ]);
+
+    expect(getOutputCodeLines(out, "bar")).toEqual([
+      "console.log(bar);",
+      "const baz = 3;",
+      "console.log(baz);",
+    ]);
+
+    expect(getOutputCodeLines(out, "baz")).toEqual(["console.log(baz);"]);
   });
 });
 
@@ -33,17 +65,25 @@ function applySkipTest(inputCode: string, props: Set<string>) {
   const [component] = getWebComponentAst(ast);
   const declaration = (component as any)?.declarations?.[0];
   const componentBody = component?.body ?? declaration?.init.body;
+  return JSON.stringify(componentBody, skipPropTransformation(props));
+}
+
+function getOutputCodeLines(out: string, byProp: string) {
   const skipped: any[] = [];
 
-  JSON.parse(
-    JSON.stringify(componentBody, skipPropTransformation(props)),
-    displaySkippedParts,
-  );
+  JSON.parse(out, displaySkippedParts);
 
   function displaySkippedParts(this: any, key: string, value: any) {
-    if (value?._skip && !this?._skip) skipped.push(value);
+    if (
+      value?.type !== "Identifier" &&
+      value?.type !== "VariableDeclarator" &&
+      value?._skip?.includes(byProp) &&
+      Array.isArray(this)
+    ) {
+      skipped.push(value);
+    }
     return value;
   }
 
-  return skipped;
+  return skipped.map((node) => generateCodeFromAST(node));
 }
