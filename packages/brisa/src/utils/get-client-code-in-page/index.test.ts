31638c96016484e42021018014d4d7be9f4abdad
Aral Roca <aral-rg@hotmail.com>
2024-07-10 22:01:37 +0200

    fix(jsx-runtime): fix key on jsx-runtime

--- packages/brisa/src/utils/get-client-code-in-page/index.test.ts
+++ packages/brisa/src/utils/get-client-code-in-page/index.test.ts
@@ -19,9 +19,9 @@ const pageWebComponents = {
 
 const i18nCode = 3072;
 const brisaSize = 5888; // TODO: Reduce this size :/
-const webComponents = 758;
+const webComponents = 792;
 const unsuspenseSize = 217;
-const rpcSize = 2445; // TODO: Reduce this size
+const rpcSize = 2446; // TODO: Reduce this size
 const lazyRPCSize = 4132; // TODO: Reduce this size
 // lazyRPC is loaded after user interaction (action, link),
 // so it's not included in the initial size
