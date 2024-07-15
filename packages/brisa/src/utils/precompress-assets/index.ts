a00694742715bdab2270c06b0005de2224fdeec7
Aral Roca <aral-rg@hotmail.com>
2024-07-15 18:31:00 +0200

    feat(build): do precompression of assets optional + improve log

--- packages/brisa/src/utils/precompress-assets/index.ts
+++ packages/brisa/src/utils/precompress-assets/index.ts
@@ -1,25 +1,40 @@
+import path from "node:path";
 import { gzipSync } from "bun";
 import { brotliCompressSync } from "node:zlib";
 import getFilesFromDir from "@/utils/get-files-from-dir";
 import { getConstants } from "@/constants";
+import { blueLog, boldLog, greenLog } from "@/utils/log/log-color";
 
 export default async function precompressAssets(assetsPath: string) {
-  const { IS_PRODUCTION } = getConstants();
+  const { LOG_PREFIX, IS_PRODUCTION, CONFIG } = getConstants();
 
-  if (!IS_PRODUCTION) return null;
+  if (!IS_PRODUCTION || !CONFIG.assetCompression) return null;
 
   const assets = await getFilesFromDir(assetsPath);
+
+  console.log(LOG_PREFIX.INFO, `Precompressing ${assets.length} assets...`);
 
   await Promise.all(
     assets.map(async (asset) => {
       if (asset.endsWith(".gz") || asset.endsWith(".br")) return;
 
       const assetContent = Bun.file(asset);
+      const assetName = asset.split(path.sep).pop() ?? asset;
       const buffer = new Uint8Array(await assetContent.arrayBuffer());
-      const gzip = gzipSync(buffer);
+      const logGzipMsg = `${greenLog("Compressed")} ${blueLog(
+        assetName,
+      )} ◆ ${boldLog("GZIP")}`;
+      const logBrotliMsg = `${greenLog("Compressed")} ${blueLog(
+        assetName,
+      )} ◆ ${boldLog("Brotli")}`;
 
-      Bun.write(`${asset}.gz`, gzip);
+      console.time(logGzipMsg);
+      Bun.write(`${asset}.gz`, gzipSync(buffer));
+      console.timeEnd(logGzipMsg);
+
+      console.time(logBrotliMsg);
       Bun.write(`${asset}.br`, brotliCompressSync(buffer));
+      console.timeEnd(logBrotliMsg);
     }),
   );
 }
