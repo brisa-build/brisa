91062f8743d8a700bb3e0cae0aa946f8e2536175
Aral Roca <aral-rg@hotmail.com>
2024-07-15 19:58:07 +0200

    chore: update brisa version

--- packages/create-brisa/create-brisa.cjs
+++ packages/create-brisa/create-brisa.cjs
@@ -28,7 +28,7 @@ rl.question("Enter project name: ", (PROJECT_NAME) => {
 
   process.chdir(PROJECT_NAME);
 
-  const BRISA_VERSION = "0.0.112";
+  const BRISA_VERSION = "0.0.113";
 
   console.log("\n🛠️  Installing brisa...\n");
 
