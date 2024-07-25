20b9baeb1715938687def64d9a5047739c5962f6
Aral Roca <aral-rg@hotmail.com>
2024-07-25 18:10:36 +0200

    feat: init adapter-vercel package

---
+++ packages/adapter-vercel/index.ts
@@ -0,0 +1,11 @@
+import type { Adapter } from 'brisa';
+
+export default function vercelAdapter(): Adapter {
+  console.log('TODO: Not implemented yet');
+  return {
+    name: 'vercel',
+    adapt(builder: any) {
+      console.log('TODO: Not implemented');
+    },
+  };
+}
