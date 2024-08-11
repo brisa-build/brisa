68e4857b51eb59aaf1644835906e3de0a1c0d697
Aral Roca <aral-rg@hotmail.com>
2024-08-11 22:11:20 +0200

    feat: resolve routes with FileSystemRouter

--- packages/brisa/src/utils/file-system-router/index.ts
+++ packages/brisa/src/utils/file-system-router/index.ts
@@ -1,7 +1,13 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
+const finalSlashIndex = new RegExp(`${path.sep}index$`);
+
 // Inspired on Bun.FileSystemRouter, but runtime-agnostic.
 export class FileSystemRouter {
   dir: string;
   fileExtensions: string[];
+  routes: Record<string, string> = {};
 
   constructor({
     dir,
@@ -12,5 +18,32 @@ export class FileSystemRouter {
   }) {
     this.dir = dir;
     this.fileExtensions = fileExtensions;
+    this.routes = this.getRoutes();
+  }
+
+  private getRoutes() {
+    const routes: Record<string, string> = {};
+    const files = fs.readdirSync(this.dir, {
+      withFileTypes: true,
+      recursive: true,
+    });
+
+    for (const file of files) {
+      if (file.isDirectory()) continue;
+      const ext = path.extname(file.name);
+      if (!this.fileExtensions.includes(ext)) continue;
+
+      const filePath = path.resolve(file.parentPath, file.name);
+      let route = filePath
+        .replace(ext, '')
+        .replace(this.dir, '')
+        .replace(finalSlashIndex, '');
+
+      if (route === '') route = '/';
+
+      routes[route] = filePath;
+    }
+
+    return routes;
   }
 }
