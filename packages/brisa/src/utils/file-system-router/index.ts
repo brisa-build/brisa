d364476de01a66b8c5a641b5816ba4ca5be491f4
Aral Roca <aral-rg@hotmail.com>
2024-08-11 23:18:36 +0200

    refactor: add cache + refactor

--- packages/brisa/src/utils/file-system-router/index.ts
+++ packages/brisa/src/utils/file-system-router/index.ts
@@ -1,16 +1,24 @@
 import path from 'node:path';
 import fs from 'node:fs';
 
-const finalSlashIndex = new RegExp(`${path.sep}index$`);
-
-// Inspired on Bun.FileSystemRouter, but runtime-agnostic.
-export function fileSystemRouter({
-  dir,
-  fileExtensions,
-}: {
+type FileSystemRouterOptions = {
   dir: string;
   fileExtensions: string[];
-}) {
+};
+
+const ENDS_WITH_SLASH_INDEX_REGEX = new RegExp(`${path.sep}index$`);
+const ROUTES_CACHE = new Map<string, Record<string, string>>();
+
+// Inspired on Bun.FileSystemRouter, but compatible with Node.js as well
+export function fileSystemRouter(options: FileSystemRouterOptions) {
+  const routes = resolveRoutes(options);
+
+  return { routes };
+}
+
+function resolveRoutes({ dir, fileExtensions }: FileSystemRouterOptions) {
+  if (ROUTES_CACHE.has(dir)) return ROUTES_CACHE.get(dir);
+
   const routes: Record<string, string> = {};
   const files = fs.readdirSync(dir, {
     withFileTypes: true,
@@ -28,14 +36,14 @@ export function fileSystemRouter({
     let route = filePath
       .replace(ext, '')
       .replace(dir, '')
-      .replace(finalSlashIndex, '');
+      .replace(ENDS_WITH_SLASH_INDEX_REGEX, '');
 
     if (route === '') route = '/';
 
     routes[route] = filePath;
   }
 
-  return {
-    routes,
-  };
+  ROUTES_CACHE.set(dir, routes);
+
+  return routes;
 }
