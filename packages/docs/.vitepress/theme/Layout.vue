16aeb6318d8625b86a8efe9309a48913d0808ef1
Daniel ARTOLA <danielofair@gmail.com>
2024-08-17 01:26:32 +0200

    docs(header-banner): add a banner to inform that docs are in beta

---
+++ packages/docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,21 @@
+<!-- .vitepress/theme/Layout.vue -->
+<template>
+  <div>
+    <HeaderBanner />
+    <Content id="brisa-content" />
+  </div>
+</template>
+
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import HeaderBanner from '../components/header-banner.vue'
+
+const { Layout } = DefaultTheme
+const Content = Layout
+</script>
+
+<style>
+#brisa-content > header{
+  margin: 13px 0;
+}
+</style>
