33fb2c0ef9eeb99fcb4e07f51fddd75121f1bb37
Daniel ARTOLA <danielofair@gmail.com>
2024-08-17 01:26:32 +0200

    docs(header-banner): add a banner to inform that docs are in beta

---
+++ packages/docs/.vitepress/components/header-banner.vue
@@ -0,0 +1,108 @@
+//banner to apper on top of the page fixed informing that the docs are in beta
+<template>
+  <div class="header-banner">
+    <div class="header-banner-content">
+      <p>
+        <span>🚧</span>
+        <span id="site">Docs are in beta. Please report any issues on </span>
+        <span id="mobile">Docs in beta. Issues on </span>
+        <a href="https://github.com/brisa-build/brisa/issues" target="_blank">GitHub</a>
+        <span>🚧</span>
+
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+.header-banner {
+  color: #3f4041;
+  text-align: center;
+  height: 24px;
+  font-size: 11px;
+  top: 0;
+  left:0;
+  width: 100vw;
+  position: fixed;
+  z-index: 1000;
+
+  background: linear-gradient(267deg, #2cebcf, #ffffff);
+  background-size: 400% 400%;
+
+  -webkit-animation: brisaBackground 20s ease infinite;
+  -moz-animation: brisaBackground 20s ease infinite;
+  animation: brisaBackground 20s ease infinite;
+}
+
+@-webkit-keyframes brisaBackground {
+  0% {
+    background-position: 0% 58%;
+  }
+  50% {
+    background-position: 100% 43%;
+  }
+  100% {
+    background-position: 0% 58%;
+  }
+}
+@-moz-keyframes brisaBackground {
+  0% {
+    background-position: 0% 58%;
+  }
+  50% {
+    background-position: 100% 43%;
+  }
+  100% {
+    background-position: 0% 58%;
+  }
+}
+@keyframes brisaBackground {
+  0% {
+    background-position: 0% 58%;
+  }
+  50% {
+    background-position: 100% 43%;
+  }
+  100% {
+    background-position: 0% 58%;
+  }
+}
+
+.header-banner-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.header-banner-content a {
+  color: #2b6cb0;
+  text-decoration: underline;
+}
+
+.header-banner-content span {
+  margin: 0 0.25rem;
+}
+
+#site {
+  display: inline;
+}
+
+#mobile {
+  display: none;
+}
+
+@media (max-width: 550px) {
+  .header-banner {
+    width: 100%;
+  }
+
+  #site {
+    display: none;
+  }
+
+  #mobile {
+    display: inline;
+  }
+}
+</style>
--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,11 +1,20 @@
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client';
+import HeaderBanner from '../components/header-banner.vue';
+import Layout from './Layout.vue';
 
 import './vars.css';
 
 export default {
   extends: DefaultTheme,
+  Layout: {
+    ...Layout,
+    components: {
+      ...Layout.components,
+      HeaderBanner,
+    },
+  },
   enhanceApp({ app }) {
     enhanceAppWithTabs(app);
   },
