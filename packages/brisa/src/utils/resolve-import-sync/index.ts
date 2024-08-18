5c38dcfe13d08075c5c209f0d761090d8ea8c803
Aral Roca <aral-rg@hotmail.com>
2024-08-18 17:56:16 +0200

    docs: add comment to resolveImportSync

--- packages/brisa/src/utils/resolve-import-sync/index.ts
+++ packages/brisa/src/utils/resolve-import-sync/index.ts
@@ -2,6 +2,10 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createRequire } from 'node:module';
 
+/**
+ * Synchronously resolves the path of an import, which is particularly useful
+ * for converting relative imports or TypeScript aliases to absolute paths.
+ */
 export default function resolveImportSync(id: string, parent?: string) {
   const req = createRequire(parent ?? path.resolve(process.cwd(), 'src'));
 
