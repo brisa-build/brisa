cdd2024d03e8a8eb116000dd44592ee3ccc74d5a
Aral Roca <aral-rg@hotmail.com>
2024-08-20 23:56:05 +0200

    test: fix tests linux

---
+++ packages/brisa/src/utils/compile-standalone-component/index.test.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'bun:test';
+
+describe('utils/compile-standalone-component', () => {
+  it('should compile a standalone server component', async () => {});
+});
--- packages/brisa/src/utils/generate-static-export/index.test.ts
+++ packages/brisa/src/utils/generate-static-export/index.test.ts
@@ -347,29 +347,29 @@ describe.each(BASE_PATHS)('utils', (basePath) => {
         expect(mockLog.mock.calls[2]).toEqual([
           constants.LOG_PREFIX.INFO,
           constants.LOG_PREFIX.TICK,
-          expect.stringContaining('/user/testUserName.html prerendered in '),
+          expect.stringContaining('/_404.html prerendered '),
         ]);
         expect(mockLog.mock.calls[3]).toEqual([
           constants.LOG_PREFIX.INFO,
           constants.LOG_PREFIX.TICK,
-          expect.stringContaining('/_404.html prerendered '),
+          expect.stringContaining('/_500.html prerendered in '),
         ]);
         expect(mockLog.mock.calls[4]).toEqual([
           constants.LOG_PREFIX.INFO,
           constants.LOG_PREFIX.TICK,
-          expect.stringContaining('/_500.html prerendered in '),
-        ]);
-        expect(mockLog.mock.calls[5]).toEqual([
-          constants.LOG_PREFIX.INFO,
-          constants.LOG_PREFIX.TICK,
           expect.stringContaining('/foo.html prerendered in '),
         ]);
-        expect(mockLog.mock.calls[6]).toEqual([
+        expect(mockLog.mock.calls[5]).toEqual([
           constants.LOG_PREFIX.INFO,
           constants.LOG_PREFIX.TICK,
           expect.stringContaining(
             '/page-with-web-component.html prerendered in ',
           ),
+        ]);
+        expect(mockLog.mock.calls[6]).toEqual([
+          constants.LOG_PREFIX.INFO,
+          constants.LOG_PREFIX.TICK,
+          expect.stringContaining('/somepage.html prerendered in '),
         ]);
         expect(mockLog.mock.calls[7]).toEqual([
           constants.LOG_PREFIX.INFO,
@@ -381,7 +381,7 @@ describe.each(BASE_PATHS)('utils', (basePath) => {
         expect(mockLog.mock.calls[8]).toEqual([
           constants.LOG_PREFIX.INFO,
           constants.LOG_PREFIX.TICK,
-          expect.stringContaining('/somepage.html prerendered in '),
+          expect.stringContaining('/user/testUserName.html prerendered in '),
         ]);
       });
 
