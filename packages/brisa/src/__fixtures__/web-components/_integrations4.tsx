460213f1011ef8f867831792cfc2f956b8c1ddf9
Aral Roca <aral-rg@hotmail.com>
2024-08-23 00:25:46 +0200

    feat: add warn log when selector is different than the defined

--- packages/brisa/src/__fixtures__/web-components/_integrations4.tsx
+++ packages/brisa/src/__fixtures__/web-components/_integrations4.tsx
@@ -2,9 +2,14 @@ import type { WebComponentIntegrations, WebContextPlugin } from 'brisa';
 import path from 'node:path';
 
 export const webContextPlugins: WebContextPlugin[] = [(ctx) => ctx];
+const pathname = `${import.meta.dirname}${path.sep}..${path.sep}lib${path.sep}some-lib.js`;
 
 export default {
   'some-lib': {
-    client: `${import.meta.dirname}${path.sep}..${path.sep}lib${path.sep}some-lib.js`,
+    client: pathname,
   },
+  'different-name': {
+    client: pathname,
+  },
+  'different-name-string-path': pathname,
 } satisfies WebComponentIntegrations;
