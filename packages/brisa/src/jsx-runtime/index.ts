8882fd4fcdc03f73cee84fc9a61c600d9fd771d0
Aral Roca <aral-rg@hotmail.com>
2024-08-25 18:31:52 +0200

    fix(render): fix render Object

--- packages/brisa/src/jsx-runtime/index.ts
+++ packages/brisa/src/jsx-runtime/index.ts
@@ -1,11 +1,10 @@
 import type { Type, Props } from '@/types';
 
 const Fragment = (props: Props) => props.children;
-const createNode = (
-  type: Type,
-  { children, ...props }: Props,
-  key?: string,
-) => Object.assign([type, { ...props, key }, children], { [Symbol.for('isJSX')]: true });
+const createNode = (type: Type, { children, ...props }: Props, key?: string) =>
+  Object.assign([type, { ...props, key }, children], {
+    [Symbol.for('isJSX')]: true,
+  });
 
 Fragment.__isFragment = true;
 
