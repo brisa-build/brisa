a64861d0a22e9f9db52bac9995771062e9828028
Aral Roca <aral-rg@hotmail.com>
2024-08-29 13:17:46 +0200

    feat: improve structure

--- packages/brisa/src/cli/serve/bun-serve/index.ts
+++ packages/brisa/src/cli/serve/bun-serve/index.ts
@@ -1,8 +1,6 @@
-import constants from '@/constants';
 import type { ServeOptions } from 'bun';
 
 export default function serve(options: ServeOptions) {
-  console.log(constants.LOG_PREFIX.INFO, `• Runtime: Bun.js ${Bun.version}`);
   const server = Bun.serve(options);
   globalThis.brisaServer = server;
 
--- packages/brisa/src/cli/serve/node-serve/index.ts
+++ packages/brisa/src/cli/serve/node-serve/index.ts
@@ -1,14 +1,9 @@
 import http from 'node:http';
 import handler from './handler';
-import constants from '@/constants';
 
 const PORT = process.env.PORT ?? 3000;
 
 export default async function serve({ port = PORT } = { port: PORT }) {
-  console.log(
-    constants.LOG_PREFIX.INFO,
-    `• Runtime: Node.js ${process.version}`,
-  );
   const server = await http.createServer(handler).listen(port);
 
   return { server, port, hostname: 'localhost' };
