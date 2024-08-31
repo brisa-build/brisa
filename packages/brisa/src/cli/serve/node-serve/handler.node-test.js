4cda43481dd598b3993da2f0d8e86c30e81d9127
Aral Roca <aral-rg@hotmail.com>
2024-08-31 19:39:41 +0200

    test: add test case

--- packages/brisa/src/cli/serve/node-serve/handler.node-test.js
+++ packages/brisa/src/cli/serve/node-serve/handler.node-test.js
@@ -46,11 +46,49 @@ describe('Node.js handler', () => {
     await new Promise((resolve) => setTimeout(resolve, 0));
     assert.strictEqual(res.getBody().includes('<title>Brisa</title><'), true);
   });
+
+  it('should redirect to the locale', async () => {
+    globalThis.mockConstants = {
+      IS_SERVE_PROCESS: true,
+      ROOT_DIR: FIXTURES_DIR,
+      SRC_DIR: path.join(FIXTURES_DIR, 'js'),
+      BUILD_DIR: path.join(FIXTURES_DIR, 'js'),
+      ASSETS_DIR: path.join(FIXTURES_DIR, 'public'),
+      PAGES_DIR: path.join(FIXTURES_DIR, 'js', 'pages'),
+      I18N_CONFIG: {
+        locales: ['en', 'es'],
+        defaultLocale: 'es',
+      },
+      LOCALES_SET: new Set(['en', 'es']),
+      CONFIG: {},
+      HEADERS: {
+        CACHE_CONTROL: 'no-cache, no-store, must-revalidate',
+      },
+      LOG_PREFIX: {},
+    };
+    const req = new http.IncomingMessage(new net.Socket());
+    req.url = '/somepage';
+    const res = createMockResponse(req);
+    const absolutePath = url.pathToFileURL(
+      path.resolve(import.meta.dirname, '../../../../server/node.js'),
+    ).href;
+    const handler = await import(absolutePath).then((m) => m.handler);
+    await handler(req, res);
+    assert.strictEqual(res.statusCode, 301);
+    assert.deepStrictEqual(res.headers, {
+      'cache-control': 'no-cache, no-store, must-revalidate',
+      expires: '-1',
+      location: '/es/somepage',
+      pragma: 'no-cache',
+      vary: 'Accept-Language',
+    });
+  });
 });
 
 function createMockResponse(req) {
   const res = new http.ServerResponse(req);
   const originalWrite = res.write.bind(res);
+  let headers = {};
   let body = '';
 
   return {
@@ -59,11 +97,21 @@ function createMockResponse(req) {
       return originalWrite(chunk);
     },
     getBody: () => body,
-    writeHead: res.writeHead.bind(res),
+    writeHead: (statusCode, statusMessage, headersObj) => {
+      if (typeof statusMessage === 'object') {
+        headersObj = statusMessage;
+        statusMessage = undefined;
+      }
+      headers = { ...headers, ...headersObj };
+      res.writeHead(statusCode, statusMessage, headersObj);
+    },
     on: res.on.bind(res),
     end: res.end.bind(res),
     off: res.off.bind(res),
     destroy: res.destroy.bind(res),
+    get headers() {
+      return headers;
+    },
     get statusCode() {
       return res.statusCode;
     },
