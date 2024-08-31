5476c3633a238d911e1dfc67b3df48d25069bcb3
Aral Roca <aral-rg@hotmail.com>
2024-08-31 15:33:26 +0200

    fix(build): solve absolute import specifiers in Node.js

--- packages/brisa/src/utils/ast/get-dependencies-list/index.tsx
+++ packages/brisa/src/utils/ast/get-dependencies-list/index.tsx
@@ -1,6 +1,7 @@
+import { getConstants } from '@/constants';
 import resolveImportSync from '@/utils/resolve-import-sync';
 import type { ESTree } from 'meriyah';
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 
 const SPECIFIERS = new Set(['ImportDefaultSpecifier', 'ImportSpecifier']);
 const AVOIDED_DEPENDENCIES = new Set(['brisa', 'brisa/server', 'brisa/client']);
@@ -30,15 +31,16 @@ export default function getDependenciesList(
 
 function resolve(path: string, base: string) {
   if (AVOIDED_DEPENDENCIES.has(path)) return;
+
+  const { CONFIG } = getConstants();
+  const isNode = CONFIG.output === 'node';
+
   try {
-    return resolveImportSync(path, base);
+    const importableFilePath = resolveImportSync(path, base);
+    return isNode ? pathToFileURL(importableFilePath).href : importableFilePath;
   } catch {
-    // It is not exactly the same, it is only the same if it has
-    // the format, otherwise it does not put the format, but
-    // import.meta.resolveSync does. The import.meta.resolveSync
-    // throws an exception if the file does not exist, but
-    // import.meta.resolve does not check if it exists and resolves
-    // the absolute path without resolving the format (.js|.ts|.tsx...)
-    return fileURLToPath(import.meta.resolve(path, base));
+    return CONFIG.output === 'node'
+      ? import.meta.resolve(path, base)
+      : fileURLToPath(import.meta.resolve(path, base));
   }
 }
