6c417b04cf3407e00c1fe5e5cfca6d72c5612119
Aral Roca <aral-rg@hotmail.com>
2024-08-31 17:28:03 +0200

    feat: add tls in Node.js via config

--- packages/brisa/src/cli/serve/node-serve/index.ts
+++ packages/brisa/src/cli/serve/node-serve/index.ts
@@ -1,11 +1,18 @@
+import https from 'node:https';
 import http from 'node:http';
 import handler from './handler';
-import constants from '@/constants';
+import constants, { getConstants } from '@/constants';
+import type { NodeTLSOptions } from '@/types';
 
 export default async function serve(
   { port = constants.PORT } = { port: constants.PORT },
 ) {
-  const server = await http.createServer(handler).listen(port);
+  const tlsOptions = getConstants().CONFIG?.tls as NodeTLSOptions;
+  const server = tlsOptions
+    ? https.createServer(tlsOptions, handler)
+    : http.createServer(handler);
+
+  server.listen(port);
 
   return { server, port, hostname: 'localhost' };
 }
