6581c505f32a66cc565a465cd7702d0daac326b8
Aral Roca <aral-rg@hotmail.com>
2024-08-31 18:42:08 +0200

    feat: use brisa.config.js in prod

--- packages/brisa/src/utils/compile-serve-into-build/index.ts
+++ packages/brisa/src/utils/compile-serve-into-build/index.ts
@@ -2,10 +2,15 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { getConstants } from '@/constants';
 import { logBuildError } from '../log/log-build';
+import getImportableFilepath from '../get-importable-filepath';
+
+const SERVER_OUTPUTS = new Set(['bun', 'node']);
 
 /**
  * This function move the brisa/cli/out/serve/index.js file into the build folder
- * executing the constants in a Macro to don't calculate them on runtime anymore.
+ * and defining the ROOT_DIR, BUILD_DIR, WORKSPACE constants.
+ *
+ * Also moves the brisa.config.js file into the build folder.
  *
  * The idea of doing this process is that they can use the build folder and run
  * the server from anywhere, now the constants are calculated at runtime from
@@ -15,14 +20,30 @@ import { logBuildError } from '../log/log-build';
 export default async function compileServeIntoBuild(
   servePathname = path.join(import.meta.dirname, 'serve', 'index.js'),
 ) {
-  const { BUILD_DIR, LOG_PREFIX, CONFIG, ROOT_DIR } = getConstants();
-  const out = path.join(BUILD_DIR, 'server.js');
+  const { BUILD_DIR, LOG_PREFIX, CONFIG, ROOT_DIR, IS_PRODUCTION } =
+    getConstants();
   const isNode = CONFIG.output === 'node';
   const runtimeName = isNode ? 'Node.js' : 'Bun.js';
   const runtimeExec = isNode ? 'node' : 'bun run';
+  const entrypoints = [];
+  const configImportPath = getImportableFilepath('brisa.config', ROOT_DIR);
+  const isServer = IS_PRODUCTION && SERVER_OUTPUTS.has(CONFIG.output ?? 'bun');
+  const serverOutPath = path.join(BUILD_DIR, 'server.js');
+
+  if (configImportPath) {
+    entrypoints.push(configImportPath);
+  }
+
+  if (isServer) {
+    entrypoints.push(servePathname);
+  }
+
+  if (!entrypoints.length) {
+    return;
+  }
 
   const output = await Bun.build({
-    entrypoints: [servePathname],
+    entrypoints,
     target: isNode ? 'node' : 'bun',
     define: {
       'process.env.ROOT_DIR': `"${ROOT_DIR}"`,
@@ -35,20 +56,29 @@ export default async function compileServeIntoBuild(
     logBuildError(`Error compiling the ${runtimeName} server`, output.logs);
   }
 
-  fs.writeFileSync(out, await output.outputs[0].text());
-  console.log(LOG_PREFIX.INFO);
-  console.log(
-    LOG_PREFIX.INFO,
-    LOG_PREFIX.TICK,
-    `${runtimeName} Server compiled into build folder`,
-  );
-  console.log(
-    LOG_PREFIX.INFO,
-    `\t- To run the ${runtimeName} server: brisa start`,
-  );
-  console.log(
-    LOG_PREFIX.INFO,
-    `\t- Or directly from the build folder: NODE_ENV=production ${runtimeExec} ${out}`,
-  );
-  console.log(LOG_PREFIX.INFO);
+  for (const file of output.outputs) {
+    const out = file.path.includes('brisa.config')
+      ? path.join(BUILD_DIR, 'brisa.config.js')
+      : serverOutPath;
+
+    fs.writeFileSync(out, await file.text());
+  }
+
+  if (isServer) {
+    console.log(LOG_PREFIX.INFO);
+    console.log(
+      LOG_PREFIX.INFO,
+      LOG_PREFIX.TICK,
+      `${runtimeName} Server compiled into build folder`,
+    );
+    console.log(
+      LOG_PREFIX.INFO,
+      `\t- To run the ${runtimeName} server: brisa start`,
+    );
+    console.log(
+      LOG_PREFIX.INFO,
+      `\t- Or directly from the build folder: NODE_ENV=production ${runtimeExec} ${serverOutPath}`,
+    );
+    console.log(LOG_PREFIX.INFO);
+  }
 }
