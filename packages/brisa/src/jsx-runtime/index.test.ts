7d8c4783bc54f8602e4250f884242aee8050bbe0
Aral Roca <aral-rg@hotmail.com>
2024-09-10 00:21:32 +0200

    test: improve test

--- packages/brisa/src/jsx-runtime/index.test.ts
+++ packages/brisa/src/jsx-runtime/index.test.ts
@@ -6,7 +6,7 @@ describe('utils', () => {
     it('should convert to object node in server-side', () => {
       const node = jsx('div', { id: 'test', children: 'Hello World' });
 
-      expect(node).toEqual(['div', { id: 'test' }, 'Hello World']);
+      expect(node).toEqual(isTransformedJSX(['div', { id: 'test' }, 'Hello World']));
     });
 
     it('should convert a nested node to object node in server-side', () => {
@@ -15,17 +15,19 @@ describe('utils', () => {
         children: jsx('span', { children: 'Hello World' }) as any,
       });
 
-      expect(node).toEqual([
+      expect(node).toEqual(isTransformedJSX([
         'div',
         { id: 'test' },
-        ['span', {}, 'Hello World'],
-      ]);
+        isTransformedJSX(['span', { }, 'Hello World']),
+      ]));
     });
 
     it('should append the "key" attribute to the props', () => {
       const node = jsx('div', { id: 'test', children: 'Hello World' }, 'key');
 
-      expect(node).toEqual(['div', { id: 'test', key: 'key' }, 'Hello World']);
+      expect(node).toEqual(isTransformedJSX(['div', { id: 'test', key: 'key' }, 'Hello World']));
     });
   });
 });
+
+const isTransformedJSX = ([type, props, children]: any) => Object.assign([type, { key: undefined, ...props}, children], { [Symbol.for('isJSX')]: true });
