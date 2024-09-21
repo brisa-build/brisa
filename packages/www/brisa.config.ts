0f67d34da4c55a315d48fffcdfbfa29d1424c29e
Aral Roca <aral-rg@hotmail.com>
2024-09-21 13:00:10 +0200

    chore: update adapter

--- packages/www/brisa.config.ts
+++ packages/www/brisa.config.ts
@@ -1,5 +1,7 @@
 import type { Configuration } from 'brisa';
+import vercel from 'brisa-adapter-vercel';
 
 export default {
   output: 'static',
+  outputAdapter: vercel(),
 } satisfies Configuration;
