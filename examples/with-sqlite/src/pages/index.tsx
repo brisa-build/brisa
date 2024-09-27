5d9f1d66cfe0719c8fa99ddaa5007a356f7ff777
Aral Roca <aral-rg@hotmail.com>
2024-09-27 12:59:51 +0200

    feat: add SQLite example

--- examples/with-sqlite/src/pages/index.tsx
+++ examples/with-sqlite/src/pages/index.tsx
@@ -1,23 +1,70 @@
-import CounterServer from '@/components/counter-server';
+import { rerenderInAction } from 'brisa/server';
+import { Database } from 'bun:sqlite';
 
+type Movie = {
+  title: string;
+  year: number;
+};
+
+const db = createDB();
+const query = db.query<Movie, Movie[]>('SELECT title, year FROM movies');
+const insertMovieQuery = db.query(
+  'INSERT INTO movies (title, year) VALUES ($title, $year)',
+);
+
+// More info about SQLite: https://bun.sh/docs/api/sqlite
 export default function Homepage() {
+  const movies = query.all();
+
+  async function addMoviesServerAction(e: FormDataEvent) {
+    const $title = e.formData.get('title') as string;
+    const $year = Number(e.formData.get('year'));
+
+    (e.target as HTMLFormElement).reset();
+    insertMovieQuery.run({ $title, $year });
+    rerenderInAction({ type: 'page' });
+  }
+
   return (
     <>
       <div class="hero">
         <h1>
-          <span class="h1_addition">Welcome to </span>Brisa
+          <span class="h1_addition">SQLite </span>Example
         </h1>
-        <p class="edit-note">✏️ Change this page on </p>
-        <code>src/pages/counter/index.tsx</code>
+        <p class="edit-note">✏️ Change the queries on </p>
+        <code>src/pages/index.tsx</code>
       </div>
 
-      <section class="counter-section">
-        <h2>Counters</h2>
-        <div class="counters">
-          <counter-client initialValue={42} />
-          <CounterServer initialValue={37} />
+      <section class="example-section">
+        <div>
+          <h2>Movies</h2>
+          {movies.length === 0 && <p>No movies found</p>}
+          <ul>
+            {movies.map((movie) => (
+              <li>
+                {movie.title} ({movie.year})
+              </li>
+            ))}
+          </ul>
         </div>
+        <form onSubmit={addMoviesServerAction}>
+          <h2>Insert a Movie</h2>
+          <input name="title" type="text" placeholder="Title"></input>
+          <input name="year" type="number" placeholder="Year"></input>
+          <button>Add movies into the DB</button>
+        </form>
       </section>
     </>
   );
+}
+
+function createDB() {
+  const db = new Database('mydb.sqlite');
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS movies (
+      title TEXT,
+      year INTEGER
+    );
+  `);
+  return db;
 }
