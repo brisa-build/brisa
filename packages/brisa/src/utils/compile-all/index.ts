9e6e35253dfabd4b094bd33a6dfc85a361eb7602
Aral Roca <aral-rg@hotmail.com>
2024-09-28 09:27:16 +0200

    feat(build): move generated css files inside public folder

--- packages/brisa/src/utils/compile-all/index.ts
+++ packages/brisa/src/utils/compile-all/index.ts
@@ -1,3 +1,6 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { getConstants } from '@/constants';
 import compileAssets from '@/utils/compile-assets';
 import compileFiles from '@/utils/compile-files';
 import { logBuildError } from '@/utils/log/log-build';
@@ -11,5 +14,25 @@ export default async function compileAll() {
     logBuildError('Failed to compile pages', logs);
   }
 
+  handleCSSFiles();
+
   return { success, logs, pagesSize };
+}
+
+function handleCSSFiles() {
+  const { BUILD_DIR } = getConstants();
+  const publicFolder = path.join(BUILD_DIR, 'public');
+  const allFiles = fs.readdirSync(BUILD_DIR);
+  const cssFilePaths: string[] = [];
+
+  for (const file of allFiles) {
+    if (!file.endsWith('.css')) continue;
+    fs.renameSync(path.join(BUILD_DIR, file), path.join(publicFolder, file));
+    cssFilePaths.push(file);
+  }
+
+  fs.writeFileSync(
+    path.join(BUILD_DIR, 'css-files.json'),
+    JSON.stringify(cssFilePaths),
+  );
 }
