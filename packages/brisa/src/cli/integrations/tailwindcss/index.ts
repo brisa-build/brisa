e28fa95ef844350a6fa58dc4d119e35598c753e3
Aral Roca <aral-rg@hotmail.com>
2024-09-30 01:08:06 +0200

    feat: integrate tailwind deps + config

--- packages/brisa/src/cli/integrations/tailwindcss/index.ts
+++ packages/brisa/src/cli/integrations/tailwindcss/index.ts
@@ -1,9 +1,68 @@
-export default function integrateTailwindCSS() {
-  console.log('TODO');
+import { getBrisaTailwindCSSDependencies } from './dependencies' with {
+  type: 'macro',
+};
+import cp from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { getConstants } from '@/constants';
+import getImportableFilepath from '@/utils/get-importable-filepath';
+import { boldLog } from '@/utils/log/log-color';
+
+const defaultTailwindCSSConfig = `import type { Configuration } from "brisa";
+import tailwindcss from 'brisa-tailwindcss';
+
+export default {
+  integrations: [tailwindcss()],
+} as Configuration;
+
+`;
+
+export default async function integrateTailwindCSS() {
+  const { ROOT_DIR, LOG_PREFIX } = getConstants();
+  const brisaConfig = getImportableFilepath('brisa.config', ROOT_DIR);
+  const dependencies = Object.entries(getBrisaTailwindCSSDependencies());
+  console.log(LOG_PREFIX.WAIT, 'Adding TailwindCSS dependencies...');
+  cp.spawnSync(
+    'bun',
+    ['add', ...dependencies.map(([name, version]) => `${name}@${version}`)],
+    {
+      stdio: 'inherit',
+    },
+  );
+
+  console.log(
+    LOG_PREFIX.INFO,
+    LOG_PREFIX.TICK,
+    'TailwindCSS dependencies added!',
+  );
+
+  if (!brisaConfig) {
+    fs.writeFileSync(
+      path.join(ROOT_DIR, 'brisa.config.ts'),
+      defaultTailwindCSSConfig,
+    );
+    console.log(
+      LOG_PREFIX.INFO,
+      LOG_PREFIX.TICK,
+      'TailwindCSS configuration added!',
+    );
+  } else {
+    console.log(
+      LOG_PREFIX.WARN,
+      `Almost there! We detected an existing ${boldLog('brisa.config.ts')} file.`,
+    );
+    console.log(
+      LOG_PREFIX.WARN,
+      'Please add the following configuration to integrate TailwindCSS:',
+    );
+    console.log(LOG_PREFIX.WARN);
+    defaultTailwindCSSConfig
+      .split('\n')
+      .forEach((line) => console.log(LOG_PREFIX.WARN, boldLog(line)));
+  }
 }
 
 if (import.meta.main) {
-  integrateTailwindCSS();
-  console.log('Tailwind integration completed!');
+  await integrateTailwindCSS();
   process.exit(0);
 }
