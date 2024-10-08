70cff8302178c71aa0444dd050faa375be11f9f6
Guillem Arias <hello@gariasf.com>
2024-10-08 11:59:19 +0200

    chore: replace emoji with icons

---
+++ packages/www/src/icons/languages.tsx
@@ -0,0 +1,22 @@
+export default function Languages({ size = 24 }: { size: number }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-languages"
+    >
+      <path d="m5 8 6 6" />
+      <path d="m4 14 6-6 2-3" />
+      <path d="M2 5h12" />
+      <path d="M7 2h1" />
+      <path d="m22 22-5-10-5 10" />
+      <path d="M14 18h6" />
+    </svg>
+  );
+}
