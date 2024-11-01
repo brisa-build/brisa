a1d227e4d67cf755092d730f94866a2bed0e225d
Latta <info@latta.ai>
2024-11-01 23:16:38 +0100

    Fix action execution without JavaScript

--- packages/brisa/src/utils/is-an-action/index.test.ts
+++ packages/brisa/src/utils/is-an-action/index.test.ts
@@ -3,9 +3,14 @@ import isAnAction from '.';
 
 describe('utils', () => {
   describe('is-an-action', () => {
-    it('should return true if the action is an action', () => {
+    it('should return true if the action is an action with actionId', () => {
       const action = () => {};
       action.actionId = '1';
+      expect(isAnAction(action)).toBe(true);
+    });
+
+    it('should return true if the action is an action with name starting with "action"', () => {
+      const action = function action1() {};
       expect(isAnAction(action)).toBe(true);
     });
 
--- packages/brisa/src/utils/is-an-action/index.ts
+++ packages/brisa/src/utils/is-an-action/index.ts
@@ -1,7 +1,5 @@
-function isAnAction(
-  action: unknown,
-): action is { actionId: string; cid: string; actions?: unknown[] } {
-  return typeof action === 'function' && 'actionId' in action;
+function isAnAction(action: unknown): action is { actionId: string; cid: string; actions?: unknown[] } {
+  return typeof action === 'function' && ('actionId' in action || action.name.startsWith('action'));
 }
 
 export default isAnAction;
