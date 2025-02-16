9c3a20aab28f71827a3235c4ad1b1e4d7247f175
Aral Roca Gomez <aral-rg@hotmail.com>
2025-02-16 18:06:54 +0100

    fix(server-action): import only valid identifiers (#757)
    * fix(server-action): import only valid identifiers
    
    * docs: revert comment

--- packages/brisa/src/utils/ast/get-var-declaration-identifiers/index.ts
+++ packages/brisa/src/utils/ast/get-var-declaration-identifiers/index.ts
@@ -10,8 +10,8 @@ export default function getVarDeclarationIdentifiers(node: ESTree.Node) {
   function getIdentifiersDependenciesOn(node: ESTree.Node) {
     const deps = new Set<string>();
 
-    JSON.stringify(node, (k, v) => {
-      if (v?.type === 'Identifier') {
+    JSON.stringify(node, function (k, v) {
+      if (v?.type === 'Identifier' && this?.property !== v) {
         deps.add(v.name);
         if (identifiers.has(v.name)) {
           for (const dep of identifiers.get(v.name)!) {
