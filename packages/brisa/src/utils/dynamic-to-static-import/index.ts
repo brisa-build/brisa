c87a6d0eb185795909c3646ada54cb9c55e95131
Aral Roca <aral-rg@hotmail.com>
2025-03-01 22:53:10 +0100

    feat: add file to be augmented in build-time

---
+++ packages/brisa/src/utils/dynamic-to-static-import/index.ts
@@ -0,0 +1,17 @@
+// This file is augmented in project build-time, adding a map like this:
+export default {
+  // middleware: middlewareModule,
+  // i18n: i18nModule,
+  // 'brisa.config': brisaConfigModule,
+  // _integrations: integrationsModule,
+  // 'css-files': cssFilesModule,
+  // actions: actionsModule,
+  // apiRoutes: {
+  //  'api/path': apiPathModule,
+  //},
+  // pages: {
+  //  'page/path': pagePathModule,
+  //},
+  apiRoutes: {},
+  pages: {},
+} 
