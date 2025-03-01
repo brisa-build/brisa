abf07bcd3d796e0b6231313c37532067a838f35e
Aral Roca <aral-rg@hotmail.com>
2025-03-01 22:55:57 +0100

    docs: improve default and comments

--- packages/brisa/src/utils/dynamic-to-static-import/index.ts
+++ packages/brisa/src/utils/dynamic-to-static-import/index.ts
@@ -1,17 +1,14 @@
-// This file is augmented in project build-time, adding a map like this:
+// This file is augmented in project build-time, replacing the null values with the actual modules.
 export default {
-  // middleware: middlewareModule,
-  // i18n: i18nModule,
-  // 'brisa.config': brisaConfigModule,
-  // _integrations: integrationsModule,
-  // 'css-files': cssFilesModule,
-  // actions: actionsModule,
-  // apiRoutes: {
-  //  'api/path': apiPathModule,
-  //},
-  // pages: {
-  //  'page/path': pagePathModule,
-  //},
+  middleware: null,
+  i18n: null,
+  'brisa.config': null,
+  _integrations: null,
+  'css-files': null,
+  actions: null,
+
+  // This is going to be augmented in project build-time to include the actual modules.
+  // Ex: { apiRoutes: { 'api/hello': [Function: hello] }, pages: { '/': [Function: Home] } }
   apiRoutes: {},
   pages: {},
-} 
+};
