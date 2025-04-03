412648662844106b32c617db14b9642a1a60e14d
Aral Roca <aral-rg@hotmail.com>
2025-04-03 22:31:42 +0200

    fix(i18n): re-load client i18n keys during navigation

--- packages/brisa/src/utils/render-to-readable-stream/index.ts
+++ packages/brisa/src/utils/render-to-readable-stream/index.ts
@@ -450,7 +450,9 @@ async function enqueueDuringRendering(
           );
 
           if (fs.existsSync(pathPageI18n)) {
-            let script = `<script data-cfasync="false" src="${compiledPagesPath}/${filenameI18n}"></script>`;
+            // Note: is necessary a different `id` each time (useId uses the request id) to ensure
+            // to re-execute this script during SPA navigation #822
+            let script = `<script id="i18n-${request.useId()}" data-cfasync="false" src="${compiledPagesPath}/${filenameI18n}"></script>`;
 
             // Script to override client translations caused by "overrideMessages" function
             if (request.store.has('_messages')) {
