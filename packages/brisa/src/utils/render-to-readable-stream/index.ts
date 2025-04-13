692352a222c7123f50f4410185036a9288bef477
Aral Roca <aral-rg@hotmail.com>
2025-04-13 20:00:08 +0200

    fix(i18n+rpc): change scripts order to work RPC at the correct moment

--- packages/brisa/src/utils/render-to-readable-stream/index.ts
+++ packages/brisa/src/utils/render-to-readable-stream/index.ts
@@ -388,6 +388,15 @@ async function enqueueDuringRendering(
       controller.hasHeadTag = true;
       controller.insideHeadTag = false;
 
+      if (filenameI18n && pathPageI18n) {
+        // Script to load the i18n page content (messages and translated pages to navigate)
+        // Note: "data-run" is necessary to ensure to re-execute this script during SPA navigation #822
+        controller.enqueue(
+          `<script data-run data-cfasync="false" src="${compiledPagesPath}/${filenameI18n}"></script>`,
+          suspenseId,
+        );
+      }
+
       // Script to unsuspense all suspense components
       if (controller.hasUnsuspense) {
         controller.enqueue(
@@ -398,14 +407,6 @@ async function enqueueDuringRendering(
       if (controller.hasActionRPC) {
         controller.enqueue(
           `<script data-cfasync="false" src="${compiledPagesPath}/_rpc-${VERSION}.js" async></script>`,
-          suspenseId,
-        );
-      }
-      if (filenameI18n && pathPageI18n) {
-        // Script to load the i18n page content (messages and translated pages to navigate)
-        // Note: "data-run" is necessary to ensure to re-execute this script during SPA navigation #822
-        controller.enqueue(
-          `<script data-run data-cfasync="false" src="${compiledPagesPath}/${filenameI18n}"></script>`,
           suspenseId,
         );
       }
