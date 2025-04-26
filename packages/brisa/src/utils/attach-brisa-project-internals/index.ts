387edc57fb01ed4d3368d89f42c500441ceaaf7d
Aral Roca <aral-rg@hotmail.com>
2025-04-26 16:39:32 +0200

    fix(build): revert plugin filter

--- packages/brisa/src/utils/attach-brisa-project-internals/index.ts
+++ packages/brisa/src/utils/attach-brisa-project-internals/index.ts
@@ -73,10 +73,16 @@ export default function attachBrisaProjectInternalsPlugin() {
   return {
     name: 'attach-brisa-project-internals',
     setup(build) {
-      build.onLoad({ filter: /.*brisa-project-internals.*/ }, ({ loader }) => ({
-        contents,
-        loader,
+      build.onResolve({ filter: /brisa-project-internals/ }, () => ({
+        path: import.meta.filename,
       }));
+      build.onLoad(
+        { filter: new RegExp(import.meta.filename) },
+        ({ loader }) => ({
+          contents,
+          loader,
+        }),
+      );
     },
   } satisfies BunPlugin;
 }
