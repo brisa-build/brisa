d26743dd7bb95f454ce61387ce962df3ab3cd143
Aral Roca <aral-rg@hotmail.com>
2025-06-05 19:11:04 +0200

    fix: remove old warning (no more need it)

--- packages/brisa/src/utils/client-build-plugin/process-client-ast/index.ts
+++ packages/brisa/src/utils/client-build-plugin/process-client-ast/index.ts
@@ -54,27 +54,11 @@ export default function processClientAst(ast: ESTree.Program, path = '') {
     }
 
     // Remove ".css" imports and log a warning
-    // TODO: Remove this restriction when this Bun feature is solved:
     // https://github.com/oven-sh/bun/issues/8280
     if (
       value?.type === 'ImportDeclaration' &&
       value?.source?.value.endsWith('.css')
     ) {
-      logWarning(
-        [
-          'CSS Global imports in web components',
-          '',
-          `Code: import '${value.source.value}';`,
-          `Path: ${path}`,
-          '',
-          'Add this global import into the layout or the page.',
-          'Currently, CSS Global imports only work in Web Components when it does not have the "skipSSR", to avoid problems, it is better not to use it here yet.',
-          '',
-          'If you have any questions or need further assistance,',
-          'feel free to contact us. We are happy to help!',
-        ],
-        'Docs: https://brisa.build/building-your-application/styling/web-components#global-styles-in-web-components',
-      );
       return null;
     }
 
